a09845dc7300efcb0c67a591c86c73bd4c0182da
Jahid Howlader <121720284+jahidhowlader@users.noreply.github.com>
2023-08-11 01:44:33 +0600

    added (#5)
    Co-authored-by: Zarif <zarifraja19@gmail.com>
    Co-authored-by: Zarif <116057351+Iam-Zarif@users.noreply.github.com>

---
+++ src/Components/Hooks.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Hooks = ({children}) => {
+    return (
+        <div>
+        <div className="max-w-[1480px] mx-auto">{children}</div>
+      </div>
+    );
+};
+
+export default Hooks;
