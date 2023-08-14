be66bd010c0c85a11f7ad5f8bb32f4dc269d4bbe
Jahid Howlader <jahidhowlader.info@gmail.com>
2023-08-15 02:02:16 +0600

    get ready for develop new UI

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,22 +1,8 @@
-import Feed from "@/Components/Feed";
-import Navbar from "@/Components/Nav";
-import Stories from "@/Components/Stories";
-
 export default function Home() {
   return(
     
     <div className='bg-gray-50 h-screen overflow-y-scroll scrollbar-hide'>
-      {/* Header */}
-
-       {/* <Nav />  */}
-      <Navbar></Navbar>
-
-      {/* Feed  */}
-      <Feed />
-
-      {/* Modal  */}
-
-      {/* <Modal /> */}
+     Get ready for project
     </div>
     
   )
