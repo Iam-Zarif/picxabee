9c71258a7e57dd45dc080d01ca240cbdca688b9d
NISHAT TASNIM <tasnim.nt00@gmail.com>
2023-08-21 01:06:44 +0600

    106

--- src/components/HomePage/feed/CommentSection.jsx
+++ src/components/HomePage/feed/CommentSection.jsx
@@ -1,38 +1,41 @@
-"use client"
-import React, { useState } from 'react';
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import {AiOutlineSend} from 'react-icons/ai'
 
-const CommentSection = ({open}) => {
-	const [comment, setComment] = useState('');
+const CommentSection = ({ open }) => {
+	const { register, handleSubmit, setValue, watch, reset } = useForm();
+	const watchComment = watch('comment', '');
 
-	const handleCommentChange = (event) => {
-		setComment(event.target.value);
-	};
-
-	const handleSubmit = (event) => {
-		event.preventDefault();
-		if (comment.trim() !== '') {
-			setComments([...comments, comment]);
-			setComment('');
+	const onSubmit = (data) => {
+		if (data.comment.trim() !== '') {
+			// Perform your submit logic here
+			reset();
 		}
+		console.log(data);
 	};
 
 	return (
-		<div className={`${ open ? " ": "hidden" }`}>
-			<form onSubmit={handleSubmit}>
-				<input
-					value={comment}
-					onChange={handleCommentChange}
+		<div className={`${open ? '' : 'hidden'}`}>
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<textarea
+					{...register('comment')}
+					value={watchComment}
+					onChange={(e) => setValue('comment', e.target.value)}
 					placeholder="Write a comment..."
-					className="border-none focus:outline-none overflow-visible min-h-8 break-words text-gray-400"
+					className="border-none focus:outline-none overflow-visible min-h-fit max-h-fit w-1/2 break-words text-gray-400 bg-slate-50 rounded-md"
 				/>
 
 				<button
 					type="submit"
-					className={`text-blue-500 font-normal ml-5 ${
-						comment ? 'visible' : 'hidden'
+					className={`font-normal  ml-5 flex justify-center  ${
+						watchComment ? 'visible' : 'hidden'
 					}`}
 				>
-					Post
+					<AiOutlineSend
+						size={26}
+						className="hover:scale-125 duration-300 hover:text-gray-400 hover:cursor-pointer"
+					/>
+				
 				</button>
 			</form>
 		</div>
