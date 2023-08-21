9d1bb2f4b07018e18f9a980ba817c100412a05a7
Zarif <zarifraja19@gmail.com>
2023-08-21 21:21:13 +0600

    added

--- src/components/LoadingSpninner/LoadingSpninner.jsx
+++ src/components/LoadingSpninner/LoadingSpninner.jsx
@@ -1,17 +1,20 @@
+/* eslint-disable jsx-a11y/alt-text */
 import { RotatingLines } from 'react-loader-spinner';
-
+import Image from 'next/image';
+import photo from "../../../public/Tuhin/UserPost/swarm.png"
 const LoadingSpninner = () => {
   return (
-    <div className='flex h-screen items-center'>
+    <div className='flex w-full h-screen items-center '>
         <div className='mx-auto'>
-        <RotatingLines
+        {/* <RotatingLines
           strokeColor="green"
           strokeWidth="4"
           animationDuration="0.75"
           width="120"
           visible={true}
         
-        />
+        /> */}
+        <Image src={photo} className='w-40 fade-in-out' alt='Logo loading'></Image>
         </div>
     </div>
   )
