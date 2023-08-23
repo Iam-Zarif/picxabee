a6ff8b24fc8a00eb6df0287826ca5d5611423f1a
Zarif <zarifraja19@gmail.com>
2023-08-23 20:10:57 +0600

    search demo

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,5 @@
 import CreatePost from "@/components/HomePage/createPost/CreatePost";
+import Posts from "@/components/HomePage/feed/Posts";
 // import Posts from "@/components/HomePage/feed/Posts";
 import Navbar from "@/components/HomePage/navbar/Navbar";
 import Stories from "@/components/HomePage/story/Stories";
@@ -19,7 +20,7 @@ export default function Home() {
         <div className="col-span-2 px-10 mt-24">
           <Stories />
           <CreatePost></CreatePost>
-          {/* <Posts></Posts> */}
+          {/* <Posts/> */}
         </div>
       </div>
       <div>
