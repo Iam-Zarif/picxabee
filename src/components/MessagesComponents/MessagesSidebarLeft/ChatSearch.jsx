3073fb3480ae445848414ab0cb4ae77812ec92a6
RF-Rezon <rezwanfaysalrezon@gmail.com>
2023-08-23 16:24:57 +0600

    basic firebase setup

--- src/components/MessagesComponents/MessagesSidebarLeft/ChatSearch.jsx
+++ src/components/MessagesComponents/MessagesSidebarLeft/ChatSearch.jsx
@@ -1,20 +1,54 @@
-
+"use client";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { useState } from "react";
+import { db } from "../../../../Firebase";
 const ChatSearch = () => {
-  return (
-    <div className='search'>
-        <div className='searchForm p-3 border-b-2 border-t-2'>
-            <input placeholder="Search Here.." type="text" className="bg-transparent border-none text-white outline-none md:text-sm text-xs p-2"/>
-        </div>
-        <div className='userChat text-white cursor-pointer md:text-base text-sm m-2'>
-            <div className="hover:bg-gray-400 rounded-md px-2 h-20 w-full flex items-center gap-3 transition-all ease-in-out">
-                <img className="w-14 h-14 object-cover rounded-full" src="https://images.pexels.com/photos/3819585/pexels-photo-3819585.jpeg" alt="" />
-                <div className='userChatInfo  flex-1'>
-                    <span className='font-bold'>Jan</span>
-                </div>
-            </div>
-        </div>
-    </div>
-  )
-}
+  const [user, setUser] = useState(null);
+  const [username, setUsername] = useState("");
+  const [error, setError] = useState(false);
 
-export default ChatSearch
+  const handleSearch = async () => {
+    const q = query(collection(db, "users"), where("displayName", "==", username));
+
+    try {
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        setUser(doc.data());
+      });
+    } catch (err) {
+      setError(true);
+    }
+  };
+
+  const handleKey = (e) => {
+    e.code === "Enter" && handleSearch();
+  };
+  return (
+    <div className="search">
+      <div className="searchForm p-3 border-b-2 border-t-2">
+        <input
+          placeholder="Search Here.."
+          type="text"
+          className="bg-transparent border-none text-white outline-none md:text-sm text-xs p-2"
+          onKeyDown={handleKey}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div className="userChat text-white cursor-pointer md:text-base text-sm m-2">
+        <div className="hover:bg-gray-400 rounded-md px-2 h-20 w-full flex items-center gap-3 transition-all ease-in-out">
+          <img
+            className="w-14 h-14 object-cover rounded-full"
+            src={user?.photoURL}
+            alt=""
+          />
+          <div className="userChatInfo  flex-1">
+            <span className="font-bold">{user?.displayName}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChatSearch;
