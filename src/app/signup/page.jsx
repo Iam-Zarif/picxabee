cd8a5d9b98033b70280e2072110696d40fff27cb
Jahid Howlader <jahidhowlader.info@gmail.com>
2023-08-25 15:57:04 +0600

    had conflict form signup form

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import SignUpForm from './SignUpForm';
 import Image from 'next/image'
+import SignUpForm from './SignupForm';
 
 const SignUpPage = () => {
     return (
