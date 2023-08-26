a8379a0569ffd965a41937813c014ef28d7515a4
HridoyHoque <hhridoy155@gmail.com>
2023-08-27 02:02:55 +0600

    Added Right Sidebar

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,19 @@
 import Feed from "@/components/HomePage/Feed/Feed";
+import MiniProfile from "@/components/HomePage/RighSidebar/MiniProfile";
+import Suggestions from "@/components/HomePage/RighSidebar/Suggestions";
 
 export default function Home() {
   return (
-    <div className="my-container">
-      <Feed />
+    <div className="my-container grid grid-cols-3">
+      <div className="col-span-2 mt-20">
+        <Feed />
+      </div>
+  
+     <div className="mt-20">
+     <MiniProfile />
+      <Suggestions />
+     </div>
+    
     </div>
   );
 }
