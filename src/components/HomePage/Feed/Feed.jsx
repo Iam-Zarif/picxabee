076b07f086e522f163198d53c394afd4ef3cc120
HridoyHoque <hhridoy155@gmail.com>
2023-08-27 02:02:55 +0600

    Added Right Sidebar

--- src/components/HomePage/Feed/Feed.jsx
+++ src/components/HomePage/Feed/Feed.jsx
@@ -3,11 +3,9 @@ import CreatePost from "./createPost/CreatePost";
 
 const Feed = () => {
   return (
-    <div className="grid grid-cols-3">
-      <div className="col-span-2">
-        <CreatePost/>
-      </div>
-    </div>
+   <>
+     <CreatePost/>
+   </>
   );
 };
 
