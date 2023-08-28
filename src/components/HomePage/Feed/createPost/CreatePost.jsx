bef4294d0f8504b55724322e12d1ee6e427e1447
TuhinKantiPal01 <tuhinkantipal886@gmail.com>
2023-08-28 23:56:42 +0600

    Complete Design Post Section

--- src/components/HomePage/Feed/createPost/CreatePost.jsx
+++ src/components/HomePage/Feed/createPost/CreatePost.jsx
@@ -1,14 +1,18 @@
 "use client";
 
 import Image from "next/image";
-import "./createpost.module.css";
+import "./CreatePost.css";
 import { useForm } from "react-hook-form";
 import axios from "axios";
-import {useRouter} from "next/navigation"
+import { useRouter } from "next/navigation";
+import { IoIosAttach } from "react-icons/io";
+import { BsImageFill, BsEmojiSmile } from "react-icons/bs";
 
 const CreatePost = () => {
 
-  const router = useRouter()
+  const router = useRouter();
+
+  // console.log(image);
 
   const {
     register,
@@ -17,148 +21,145 @@ const CreatePost = () => {
     formState: { errors },
   } = useForm();
 
-  const onSubmit = (data) => {
-
+  const onSubmit = async (data) => {
     // console.log(data);
     // console.log(data.text);
     // console.log(data.image.length);
+    const { text } = data;
 
     const image = data.image[0];
     const formData = new FormData();
     formData.append("image", image);
     const url = `https://api.imgbb.com/1/upload?key=f3218173624c8aebe56d3c415677e482`;
-    const { text } = data;
 
     if (data.image.length == 0) {
-      fetch("http://localhost:3000/api/posts", {
-        method: "POST",
-        headers: {
-          "content-type": "application/json",
-        },
-        body: JSON.stringify({
-          author: {
-            email:"jahid@gmail.com",
-            name: "Jahid Howlader",
-            profile_picture: "https://i.ibb.co/dK0NCr6/Jahid-Howlader.jpg"
+      try {
+        const res = await fetch("http://localhost:3000/api/posts", {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
           },
-          caption: text,
-          postImage: '',
-          comments: [
-            {
+          body: JSON.stringify({
             author: {
-              name: "",
-              profile_picture: ""
+              email: "jahid@gmail.com",
+              name: "Jahid Howlader",
+              profile_picture: "https://i.ibb.co/dK0NCr6/Jahid-Howlader.jpg",
             },
-            }
-          ]
-        }),
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          reset()
-          if (data.insertedId) {
-            reset();
-            router.refresh();
-          }
+            content: text || "",
+            image: "",
+            comments: [
+              {
+                author: {
+                  name: "",
+                  profile_picture: "",
+                },
+              },
+            ],
+          }),
         });
+        if (res.ok) {
+          alert("Post Create Successful");
+          router.refresh();
+        } else {
+          throw new Error("Failed to create post");
+        }
+      } catch (error) {
+        console.log(error);
+      }
     }
 
     if (data.image.length == 1) {
-
-      axios.post(url, formData)
-        .then(imageData => {
-
-          if (imageData.status === 200) {
-
-            // console.log(imageData.data.data.url);
-            fetch("http://localhost:3000/api/posts", {
+      axios.post(url, formData).then(async (imageData) => {
+        // setImage(imageData.data.data.url);
+        if (imageData.status === 200) {
+          try {
+            const res = await fetch("http://localhost:3000/api/posts", {
               method: "POST",
               headers: {
                 "content-type": "application/json",
               },
               body: JSON.stringify({
                 author: {
-                  email:"jahid@gmail.com",
+                  email: "jahid@gmail.com",
                   name: "Jahid Howlader",
-                  profile_picture: "https://i.ibb.co/dK0NCr6/Jahid-Howlader.jpg"
+                  profile_picture: "https://i.ibb.co/dK0NCr6/Jahid-Howlader.jpg",
                 },
                 content: text || "",
                 image: imageData.data.data.url,
                 comments: [
                   {
                     author: {
-                        email: "",
-                        name: "",
-                        profile_picture: "",
+                      name: "",
+                      profile_picture: "",
                     },
-                    comment: "",
-                  }
-                ]
+                  },
+                ],
               }),
-            })
-              .then((res) => res.json())
-              .then((data) => {
-                reset()
-                // console.log(data);
-                // if (data.insertedId) {
-                //   reset();
-                // }
-              });
+            });
+            if (res.ok) {
+              alert("Success");
+              router.refresh();
+            } else {
+              throw new Error("Failed to fetch");
+            }
+          } catch (error) {
+            console.log(error);
           }
-        })
+        }
+      });
     }
   };
 
   return (
     <>
-      <div className="mt-10">
-        <div className="flex gap-x-5 bg-[#F9F5F6] px-10 py-5">
-          <Image
-            src="https://i.ibb.co/p4cD0Gs/good-deal-right-corner-confident-pleasant-friendly-looking-african-american-gorgeous-woman-with-afro.jpg"
-            className="w-10 h-10 rounded-[50%] object-fit"
-            alt="img"
-            width={30}
-            height={30}
-          />
-
-          <div>
-            <form 
-            onSubmit={handleSubmit(onSubmit)}
-            >
-              <textarea
-                className="wonderful-textarea"
-                rows="4"
-                cols="100"
-                name="text"
-                placeholder="Enter your text here..."
-                {...register("text")}
-              ></textarea>
-
-              <div className="flex justify-center gap-x-5 my-8">
-                <div className="flex gap-x-2 items-center">
-                  <div className="image-upload-container">
-                    <input type="file" id="image-input" accept="image/*" 
-                    {...register("image")} 
-                    />
-                    <label htmlFor="image-input" className="upload-button">
-                      Upload Image
-                    </label>
-                  </div>
-                </div>
-              </div>
-
-              <div>
-                <button
-                  type="submit"
-                  className="btn btn-primary block rounded-none w-full text-white"
-                >
-                  POST
-                </button>
-              </div>
-            </form>
-          </div>
+      <section className="relative bg-[#D2D2D2] p-4 bg-opacity-75 shadow-sm w-[90%] mx-auto mt-10">
+        <div className="">
+          <h1 className="text-center font-semibold text-lg py-2">Create a Post</h1>
         </div>
-      </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="px-5">
+            <textarea
+              name="text"
+              {...register("text")}
+              id=""
+              cols="30"
+              rows="8"
+              className="w-full resize-none p-3 text-md rounded-md focus:outline-none focus:shadow-lg"
+              placeholder="What's Your Mind"
+            ></textarea>
+            
+            <div className="flex justify-between px-10 py-5">
+              <div className="flex gap-x-2">
+                <label class="custom-file-upload">
+                  {/* <input type="image" /> */}
+                  <BsImageFill size={22} />
+                </label>
+                <label class="custom-file-upload">
+                  <input type="file" id="image-input" accept="image/*" {...register("image")} />
+                  <IoIosAttach size={22} />
+                </label>
+              </div>
+              <div>
+                <BsEmojiSmile size={22} />
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-between items-center">
+            <div></div>
+            <div>
+              <div className="form-control w-full max-w-xs flex ">
+                <select className="select select-bordered rounded-none">
+                  <option selected>Public</option>
+                  <option>Private</option>
+                </select>
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-end mt-5">
+            <button className="btn btn-info rounded-none font-semibold">Create Post</button>
+          </div>
+        </form>
+      </section>
     </>
   );
 };
