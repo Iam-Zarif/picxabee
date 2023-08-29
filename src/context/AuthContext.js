045c5b3e9ceb968bbcf3a264084164875722c1cb
Jahid Howlader <jahidhowlader.info@gmail.com>
2023-08-29 12:29:23 +0600

    added env file

--- src/context/AuthContext.js
+++
@@ -1,7 +0,0 @@
-"use client"
-
-import { createContext } from "react"
-
-const AuthContext = createContext(null)
-
-export default AuthContext
--- src/firebase/firebase.auth.js
+++
@@ -1,8 +0,0 @@
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import app from "./firebase.config";
-
-const auth = getAuth(app)
-
-export const googleProvider = new GoogleAuthProvider();
-
-export default auth
