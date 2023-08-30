530c6df9ae3b579750db0e37a8412fcad2e422f1
NISHAT TASNIM <tasnim.nt00@gmail.com>
2023-08-30 12:02:24 +0600

    postCard

--- src/components/HomePage/Feed/postCard/PostCards.jsx
+++ src/components/HomePage/Feed/postCard/PostCards.jsx
@@ -1,33 +1,42 @@
-'use client'
+'use client';
 import SinglePost from './SinglePost';
 import useSWR from 'swr';
 
-async function getData() {
-	const res = await fetch('http://localhost:3000/api/posts', {
-		cache: 'no-store',
-	});
-	if (!res.ok) {
-		throw new Error('Failed to fetch data');
-	}
-	return res.json();
-}
+// async function getData() {
+// 	const res = await fetch('https://feed-silk.vercel.app/api/posts', {
+// 		cache: 'no-store',
+// 	});
+// 	if (!res.ok) {
+// 		throw new Error('Failed to fetch data');
+// 	}
+// 	return res.json();
+// }
 
 const PostCards = () => {
-
 	const fetcher = (...args) => fetch(...args).then((res) => res.json());
-	const { data: posts, error, isLoading } = useSWR('/api/posts', fetcher);
+	const {
+		data: posts,
+		error,
+		isLoading,
+	} = useSWR('/api/posts', fetcher, {
+		refreshInterval: 1000,
+	});
+
 	if (error) return <div>failed to load</div>;
 	if (isLoading) return <div>loading...</div>;
 
 	// const posts = await getData();
-	console.log(posts);
+	// console.log(posts);
 
 	return (
 		<>
 			<div className="pb-28 pt-12 mx-auto w-full h-screen">
-				{posts && posts?.map((post) => (
-					<SinglePost key={post._id} post={post}></SinglePost>
-				))}
+				{posts &&
+					posts
+						?.reverse()
+						.map((post) => (
+							<SinglePost key={post._id} post={post}></SinglePost>
+						))}
 			</div>
 		</>
 	);
