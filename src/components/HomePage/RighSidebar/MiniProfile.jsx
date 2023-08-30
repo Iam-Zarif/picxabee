63ec52c152258e7ed89f7e22ef4f8c92319c642d
HridoyHoque <hhridoy155@gmail.com>
2023-08-31 00:46:20 +0600

    NC-22 Created profile Page

--- src/components/HomePage/RighSidebar/MiniProfile.jsx
+++ src/components/HomePage/RighSidebar/MiniProfile.jsx
@@ -3,7 +3,7 @@ import avatar from "/public/admin.jpg";
 
 const MiniProfile = () => {
     return (
-        <div className="flex items-center justify-between mt-14 ml-10">
+        <div className="flex items-center justify-between ml-10">
           <Image className="w-16 h-16 rounded-full p-[2px]" src={avatar} alt="" />
 
             <div className="flex-1 mx-4">
