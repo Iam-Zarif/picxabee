b5b9527249520bc285aa351eec73561499280900
Jahid Howlader <jahidhowlader.info@gmail.com>
2023-09-01 12:42:52 +0600

    bug fixed auth

--- src/app/auth/register/RegisterForm.jsx
+++ src/app/auth/register/RegisterForm.jsx
@@ -1,6 +1,7 @@
 "use client";
 import AuthContext from "@/context/AuthContext";
 import { db, storage } from "@/firebase/firebase.config";
+import useAuth from "@/hooks/useAuth";
 import { updateProfile } from "firebase/auth";
 import { doc, setDoc } from "firebase/firestore";
 import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
@@ -10,10 +11,10 @@ import { useForm } from "react-hook-form";
 import { toast } from "react-hot-toast";
 
 const RegisterForm = () => {
-  const { createUser, profileUpdate } = useContext(AuthContext);
+  // const { createUser, updateUser } = useContext(AuthContext);
   const navigate = useRouter();
 
-  // const { createUser, updateUser, user, setLoading } = useAuth()
+  const { createUser, updateUser, user, setLoading } = useAuth()
   const {
     register,
     handleSubmit,
@@ -27,42 +28,75 @@ const RegisterForm = () => {
   const from = search.get("redirectUrl") || "/";
   const { replace, refresh } = useRouter();
 
-  const onSubmit = async (data) => {
-    // const { name, email, password, photo } = data;
-    const { name, email, password, photoURL } = data;
-    console.log(data)
-    const toastId = toast.loading("Loading...");
-    // setLoading(true);
+  const uploadImage = async (event) => {
+
+    const formData = new FormData();
+    if (!event.target.files[0]) return;
+    formData.append("image", event.target.files[0]);
 
     try {
-      //   await createUser(email, password)    // >>>>>>>>>>>> JAHID VAI CODE
-      //     .then(async () => {
-      //       reset();
-      //       await updateUser(name, "https://i.ibb.co/7b2bX9K/m-h-blog-img-1.jpg")
-      //         .then(async () => {
-      //           console.log("upload images");
-      //           await fetch("http://localhost:3000/api/users", {
-      //             method: "POST",
-      //             body: JSON.stringify({
-      //               name,
-      //               email,
-      //               bio: "",
-      //               followers: 0,
-      //               following: 0,
-      //               posts: 0,
-      //               profile_picture: "https://i.ibb.co/7b2bX9K/m-h-blog-img-1.jpg",
-      //             }),
-      //           })
-      //             .then(() => console.log("User Creat on MongoDB"))
-      //             .catch((err) => console.log(err));
+      const res = await fetch(
+        `https://api.imgbb.com/1/upload?key=${process.env.NEXT_PUBLIC_IMGBB_STOREIMG}`,
+        {
+          method: "POST",
+          body: formData,
+        }
+      );
+      if (!res.ok) throw new Error("Failed to upload image")
 
-      //           alert("Successfully Signin");
-      //           setLoading(false);
-      //           navigate.push("/");
-      //         })
-      //         .catch((err) => console.log(err));
-      //     })
-      //     .catch((err) => alert(err.code));
+      const data = await res.json();
+      setValue("photo", data.data.url);
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const onSubmit = async (data) => {
+    const { name, email, password, photo } = data;
+    // const { name, email, password, photoURL } = data;
+    console.log(data)
+    const toastId = toast.loading("Loading...");
+    setLoading(true);
+
+    try {
+      createUser(email, password)
+        .then(() => {
+          reset();
+
+          updateUser(name, photo)
+            .then(() => {
+              console.log("upload images");
+
+              fetch("http://localhost:3000/api/users", {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                  name,
+                  email,
+                  bio: "",
+                  followers: 0,
+                  following: 0,
+                  posts: 0,
+                  profile_picture: photo || '',
+                  role: 'user'
+                }),
+              })
+                .then(() => console.log("User Creat on MongoDB"))
+                .catch((err) => console.log(err));
+
+              alert("Successfully Register");
+              setLoading(false);
+              navigate.push("/");
+            })
+            .catch((err) => console.log(err));
+        })
+        .catch((err) => alert(err.code));
+
+
+
       const res = await createUser(email, password);
       const storageRef = ref(storage, name);
 
@@ -73,7 +107,6 @@ const RegisterForm = () => {
       uploadTask.on(
         "state_changed",
         (snapshot) => {
-          // You can track the upload progress here if needed
         },
         (error) => {
           console.error(error);
@@ -122,9 +155,8 @@ const RegisterForm = () => {
         name="name"
         placeholder="Name"
         {...register("name", { required: true })}
-        className={`block mt-3 p-3 border border-primary-color outline-primary-color rounded-md w-full bg-transparent ${
-          errors.name ? "border-red focus:border-red focus:outline-red" : ""
-        }`}
+        className={`block mt-3 p-3 border border-primary-color outline-primary-color rounded-md w-full bg-transparent ${errors.name ? "border-red focus:border-red focus:outline-red" : ""
+          }`}
       />
       {errors.name?.type === "required" && <span className="text-red font-semibold">Name is required</span>}
 
@@ -138,9 +170,8 @@ const RegisterForm = () => {
           pattern:
             /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
         })}
-        className={`block mt-3 p-3 border border-primary-color outline-primary-color rounded-md w-full bg-transparent ${
-          errors.email ? "border-red focus:border-red focus:outline-red" : ""
-        }`}
+        className={`block mt-3 p-3 border border-primary-color outline-primary-color rounded-md w-full bg-transparent ${errors.email ? "border-red focus:border-red focus:outline-red" : ""
+          }`}
       />
       {errors.email?.type === "required" && <span className="text-red font-semibold">Email is required</span>}
       {errors.email?.type === "pattern" && (
@@ -153,9 +184,8 @@ const RegisterForm = () => {
         name="password"
         placeholder="Password"
         {...register("password", { required: true, pattern: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9]).{8}/ })}
-        className={`block mt-3 p-3 border border-primary-color outline-primary-color rounded-md w-full bg-transparent ${
-          errors.password ? "border-red focus:border-red focus:outline-red" : ""
-        }`}
+        className={`block mt-3 p-3 border border-primary-color outline-primary-color rounded-md w-full bg-transparent ${errors.password ? "border-red focus:border-red focus:outline-red" : ""
+          }`}
       />
       {errors.password?.type === "required" && <span className="text-red font-semibold">Password is required</span>}
       {errors.password?.type === "pattern" && (
@@ -163,20 +193,21 @@ const RegisterForm = () => {
       )}
 
       {/* ImageURL Input */}
-      <input
+      <input onChange={uploadImage} type="file" className={`block mt-3 p-3 border border-primary-color outline-primary-color rounded-md w-full bg-transparent ${errors.photo ? "border-red focus:border-red focus:outline-red" : ""}`} />
+
+      {/* TODO: This is not working right now.. */}
+      {/* <input
         type="file"
-        // onChange={upoloadImage}
+        onChange={uploadImage}
         name="photo"
         placeholder="Photo Url"
         {...register("photo")}
-        className={`block mt-3 p-3 border border-primary-color outline-primary-color rounded-md w-full bg-transparent ${
-          errors.password ? "border-red focus:border-red focus:outline-red" : ""
-        }`}
-      />
-      {errors.photo?.type === "required" && <span className="text-red font-semibold">Photo is required</span>}
+        className={`block mt-3 p-3 border border-primary-color outline-primary-color rounded-md w-full bg-transparent ${errors.photo ? "border-red focus:border-red focus:outline-red" : ""}`}
+      /> 
+      {errors.photo?.type === "required" && <span className="text-red-500 font-semibold">Photo is required</span>} */}
 
       {/* Submit */}
-      <input type="submit" className="bg-primary-color w-full text-white rounded-md p-3 cursor-pointer mt-3" />
+      <input type="submit" className="bg-black w-full text-white rounded-md p-3 cursor-pointer mt-3" />
     </form>
   );
 };
