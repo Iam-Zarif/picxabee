96e90a3c514d543e0c574ba581ee269c53ea7b92
NISHAT TASNIM <tasnim.nt00@gmail.com>
2023-09-03 01:25:23 +0600

    df

--- src/app/auth/register/RegisterForm.jsx
+++ src/app/auth/register/RegisterForm.jsx
@@ -135,8 +135,8 @@ const RegisterForm = () => {
         placeholder="Email"
         {...register("email", {
           required: true,
-          pattern:
-            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+          // pattern:
+          //   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
         })}
         className={`block mt-3 p-3 border border-primary-color outline-primary-color rounded-md w-full bg-transparent ${
           errors.email ? "border-red focus:border-red focus:outline-red" : ""
