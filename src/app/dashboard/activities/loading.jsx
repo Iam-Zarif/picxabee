15c5a2ff1d32dacd4631d3e264d083a006984388
NISHAT TASNIM <tasnim.nt00@gmail.com>
2023-09-02 19:36:18 +0600

    picxa

--- src/app/dashboard/activities/loading.jsx
+++ src/app/dashboard/activities/loading.jsx
@@ -3,7 +3,7 @@ import { ClimbingBoxLoader } from 'react-spinners'
 
 const Loading = () => {
   return (
-    <div className=' '>
+    <div>
         <ClimbingBoxLoader className='mx-auto' size={60} color="#FFFFFF" />
     </div>
   )
