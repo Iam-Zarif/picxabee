04f48b83ace529470cd1b30fc1f1079c7c74b7a0
Jahid Howlader <jahidhowlader.info@gmail.com>
2023-09-05 00:18:37 +0600

    responsive story

--- src/components/HomePage/RighSidebar/Suggestions.jsx
+++ src/components/HomePage/RighSidebar/Suggestions.jsx
@@ -6,10 +6,10 @@ import Image from "next/image";
 const Suggestions = () => {
 
     const fetcher = (...args) => fetch(...args).then((res) => res.json());
-	const { data  } = useSWR('/api/users', fetcher)
+    const { data } = useSWR('/api/users', fetcher)
     console.log(data)
-     const SuggestedUsers = data && data.slice(0, 6) 
-   
+    const SuggestedUsers = data && data.slice(0, 6)
+
     return (
         <div className="mt-4 ml-10">
             <div className="flex justify-between text-sm mb-5">
@@ -22,14 +22,14 @@ const Suggestions = () => {
                     key={idx}
                     className="flex items-center justify-between mt-3"
                 >
-                  <div className='flex'>
-                  <Image height={40} width={40} className="rounded-full border p-[2px]" src={user?.profile_picture} alt="" />
-                    <h2 className="font-semibold text-sm">{user?.name}</h2>
-                  </div>
+                    <div className='flex items-center gap-2'>
+                            <Image height={40} width={40} className="w-10 h-10 rounded-full border p-[2px]" src={user?.profile_picture} alt="" />
+                        <h2 className="font-semibold text-sm">{user?.name}</h2>
+                    </div>
                     <div className=" ml-4 ">
-                       
+
                         {/* <h3 className="text-xs text-gray-400">{user?.role}</h3> */}
-                    <button className="text-red-400 text-sm">Follow</button>
+                        <button className="text-red-400 text-sm">Follow</button>
                     </div>
                 </div>
             ))}
