396e504db6ee8138ff75ca15d388909b041e7ee4
Jahid Howlader <jahidhowlader.info@gmail.com>
2023-09-05 13:06:32 +0600

    added admin dashboard layout

--- src/app/dashboard/feedback/page.js
+++ src/app/dashboard/feedback/page.js
@@ -1,75 +1,70 @@
-"use client"
-import Image from 'next/image';
-import React, { useEffect, useState } from 'react'
-import useSWR from 'swr';
-import Loading from '../activities/loading';
+
 
 const FeedbackPage = () => {
-	const fetcher = (...args) => fetch(...args).then((res) => res.json());
-	const { data: feedbacks } = useSWR('/api/feedbacks', fetcher, {
-		refreshInterval: 1000,
-	});
-	console.log(feedbacks);
+// 	const fetcher = (...args) => fetch(...args).then((res) => res.json());
+// 	const { data: feedbacks } = useSWR('/api/feedbacks', fetcher, {
+// 		refreshInterval: 1000,
+// 	});
+// 	console.log(feedbacks);
 	
 
 	
 
-	// console.log(formattedDateTime);
-	const [isLoading, setIsLoading] = useState(true);
-  useEffect(() => {
-    // Simulate an asynchronous operation (e.g., fetching data)
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 2000); // Simulated 2-second delay
-  }, []);
-	const formatDate = (createdAt) => {
-		const inputDate = new Date(createdAt);
-		const options = { dateStyle: 'long', timeStyle: 'medium' };
-		const formattedDateTime = inputDate.toLocaleString(undefined, options);
-		return formattedDateTime;
-	};
+// 	const [isLoading, setIsLoading] = useState(true);
+//   useEffect(() => {
+//     setTimeout(() => {
+//       setIsLoading(false);
+//     }, 2000); 
+//   }, []);
+// 	const formatDate = (createdAt) => {
+// 		const inputDate = new Date(createdAt);
+// 		const options = { dateStyle: 'long', timeStyle: 'medium' };
+// 		const formattedDateTime = inputDate.toLocaleString(undefined, options);
+// 		return formattedDateTime;
+// 	};
 
 	return (
-		<div className="w-10/12 mb-60 ml-auto mr-20 pt-8 mt-20 z-0">
-			{
-				isLoading ? <><Loading/></>
-				:
-				<><div>
-				{feedbacks?.map((feedback) => (
-					<div
-						key={feedback._id}
-						className="collapse collapse-arrow join-item border border-base-300"
-					>
-						<input type="radio" name="my-accordion-4" checked="checked" />
-						<div className="glass collapse-title text-xl font-medium">
-							<div className="flex items-center">
-								<Image
-									src={feedback?.author?.profile_picture}
-									width={50}
-									height={50}
-									alt="Picture of the author"
-									className="rounded-full h-12 w-12 object-cover border p-1 mr-3"
-								/>
-								<div>
-									<p className="font-semibold capitalize">
-										{feedback?.author?.name}
-									</p>
-									<p className="font-light text-sm">
-										{formatDate(feedback?.createdAt)}
-									</p>
-								</div>
-							</div>
-						</div>
-						<div className="collapse-content">
-							<h1 className="px-5 py-3">{feedback?.feedback}</h1>
-						</div>
-					</div>
-				))}
-			</div></>
-			}
+		// <div className="w-10/12 mb-60 ml-auto mr-20 pt-8 mt-20 z-0">
+		// 	{
+		// 		isLoading ? <><Loading/></>
+		// 		:
+		// 		<><div>
+		// 		{feedbacks?.map((feedback) => (
+		// 			<div
+		// 				key={feedback._id}
+		// 				className="collapse collapse-arrow join-item border border-base-300"
+		// 			>
+		// 				<input type="radio" name="my-accordion-4" checked="checked" />
+		// 				<div className="glass collapse-title text-xl font-medium">
+		// 					<div className="flex items-center">
+		// 						<Image
+		// 							src={feedback?.author?.profile_picture}
+		// 							width={50}
+		// 							height={50}
+		// 							alt="Picture of the author"
+		// 							className="rounded-full h-12 w-12 object-cover border p-1 mr-3"
+		// 						/>
+		// 						<div>
+		// 							<p className="font-semibold capitalize">
+		// 								{feedback?.author?.name}
+		// 							</p>
+		// 							<p className="font-light text-sm">
+		// 								{formatDate(feedback?.createdAt)}
+		// 							</p>
+		// 						</div>
+		// 					</div>
+		// 				</div>
+		// 				<div className="collapse-content">
+		// 					<h1 className="px-5 py-3">{feedback?.feedback}</h1>
+		// 				</div>
+		// 			</div>
+		// 		))}
+		// 	</div></>
+		// 	}
 			
 			
-		</div>
+		// </div>
+		<></>
 	);
 };
 
---
+++ src/components/ad/Ad.jsx
@@ -0,0 +1,17 @@
+"use client"
+import Lottie from "lottie-react";
+import pc from './pc.json'
+import socialmedia from './socialmedia.json'
+
+const Ad = () => {
+    return (
+        <div>
+            <div>
+                
+                <Lottie animationData={socialmedia}></Lottie>
+            </div>
+        </div>
+    );
+};
+
+export default Ad;
