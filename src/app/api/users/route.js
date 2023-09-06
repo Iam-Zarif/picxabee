0f3a23a38b76eb1bb7bc0ff476318796e8f585da
NISHAT TASNIM <tasnim.nt00@gmail.com>
2023-09-06 20:12:01 +0600

    donation option addedd

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,33 +1,59 @@
-import User from "@/models/User"
-import connect from "@/utils/db"
-import { NextResponse } from "next/server"
-
+import User from '@/models/User';
+import connect from '@/utils/db';
+import { NextResponse } from 'next/server';
 
 export const GET = async () => {
-
-    try {
-        await connect()
-        const users = await User.find()
-        return new NextResponse(JSON.stringify(users), { status: 200 })
-
-    } catch (err) {
-        return new NextResponse('User Fetch Problems', { status: 500 })
-    }
-}
+	try {
+		await connect();
+		const users = await User.find();
+		return new NextResponse(JSON.stringify(users), { status: 200 });
+	} catch (err) {
+		return new NextResponse('User Fetch Problems', { status: 500 });
+	}
+};
 
 export const POST = async (request) => {
+	const body = await request.json();
+	const newUser = new User(body);
 
-    const body = await request.json()
-    const newUser = new User(body)
+	try {
+		await connect();
+		await newUser.save();
+		return new NextResponse('User has been created', { status: 201 });
+	} catch (err) {
+		return new NextResponse('Database Error', { status: 500 });
+	}
+};
 
-    try {
-        await connect()
-        await newUser.save()
-        return new NextResponse("User has been created", { status: 201 });
+export const PATCH = async (request) => {
+	try {
+		const userId = request.nextUrl.searchParams.get('userId');
+		const postId = request.nextUrl.searchParams.get('postId');
+		await connect();
 
-    } catch (err) {
-        return new NextResponse("Database Error", { status: 500 });
-    }
-}
+		let savePost;
+		if (postId && userEmail) {
+			// Check if the user has already liked the post
+			const user = await User.findById(userId);
 
+			if (!user) {
+				return new NextResponse(
+					JSON.stringify({ message: 'User not found' }, { status: 404 })
+				);
+			}
+			const hasSaved = user.save_items.some((item) => item._id === postId);
+			console.log(hasSaved);
 
+			if (hasLiked) {
+				
+			} else {
+				user.save_items.push(postId);
+			}
+		}
+	} catch (error) {
+		return NextResponse.json(
+			{ message: 'Internal server error' },
+			{ status: 500 }
+		);
+	}
+};
