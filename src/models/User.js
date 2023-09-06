b253645ef936a188f540e2298c4cb45231e59671
NISHAT TASNIM <tasnim.nt00@gmail.com>
2023-09-06 20:43:25 +0600

    donation option added

--- src/models/User.js
+++ src/models/User.js
@@ -1,16 +1,15 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const { Schema } = mongoose;
 
 let User;
 
 try {
-  // Try to get the existing model if it exists
-  User = mongoose.model("User");
+	// Try to get the existing model if it exists
+	User = mongoose.model('User');
 } catch {
-  // If the model doesn't exist, define it
-<<<<<<< HEAD
-  const userSchema = new Schema(
+	// If the model doesn't exist, define it
+	const userSchema = new Schema(
 		{
 			name: {
 				type: String,
@@ -36,49 +35,16 @@ try {
 			profile_picture: {
 				type: String,
 			},
-			save_items: {
-				type: Array,
+			role: {
+				type: String,
 			},
 		},
 		{
 			timestamps: true,
 		}
 	);
-=======
-  const userSchema = new Schema({
-    name: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      unique: true,
-      required: true,
-    },
-    bio: {
-      type: String,
-    },
-    followers: {
-      type: Number,
-    },
-    following: {
-      type: Number,
-    },
-    posts: {
-      type: Number,
-    },
-    profile_picture: {
-      type: String,
-    },
-    role: {
-      type: String,
-    },
-  }, {
-    timestamps: true,
-  });
->>>>>>> 3054a8c2482fe9a2ee805bbfc0f69bbc581ac1ff
 
-  User = mongoose.model("User", userSchema);
+	User = mongoose.model('User', userSchema);
 }
 
 export default User;
