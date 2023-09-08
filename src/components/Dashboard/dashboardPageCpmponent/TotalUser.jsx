413e5cd82948303f7b0b03dded6cafec6e9482ff
RF-Rezon <rezwanfaysalrezon@gmail.com>
2023-09-08 15:52:01 +0600

    User Dashboard Edited but problem with nav when responsive

--- src/components/Dashboard/dashboardPageCpmponent/TotalUser.jsx
+++ src/components/Dashboard/dashboardPageCpmponent/TotalUser.jsx
@@ -9,8 +9,8 @@ const TotalUser = () => {
     const { data, error, isLoading } = useFetchData('/api/users')
 
     return (
-        <div className='h-[150px] bg-red bg-opacity-30 rounded-md flex items-center justify-start'>
-        <div className="text-white font-semibold text-start space-y-4 pl-6">
+        <div className='flex-1 px-4 py-6 bg-red bg-opacity-30 rounded-md flex items-center justify-start'>
+        <div className="text-white font-semibold text-start md:text-start space-y-4 pl-6">
             <p className="text-sm">Total Users</p>
             <div className="flex items-center justify-between gap-x-3">
             
