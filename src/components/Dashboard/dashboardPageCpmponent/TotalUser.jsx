9357ec9c5c61885120bf9e8024292db2ca0cd96c
RF-Rezon <rezwanfaysalrezon@gmail.com>
2023-09-08 13:02:42 +0600

    total post ui changed

--- src/components/Dashboard/dashboardPageCpmponent/TotalUser.jsx
+++ src/components/Dashboard/dashboardPageCpmponent/TotalUser.jsx
@@ -8,7 +8,7 @@ const TotalUser = () => {
     const { data, error, isLoading } = useFetchData('/api/users')
 
     return (
-        <div className="h-[150px] bg-red bg-opacity-10 rounded-md flex items-center justify-center">
+        <div className="h-[150px] bg-blue-400 bg-opacity-10 rounded-md flex items-center justify-center">
             <h3 className="text-2xl font-semibold">Total User: {data?.length}</h3>
         </div>
     );
