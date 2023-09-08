c742f35830999016390aa4f2fe3beeb32ebb1885
RF-Rezon <rezwanfaysalrezon@gmail.com>
2023-09-08 18:29:45 +0600

    dashboard edited rej

--- src/components/Dashboard/dashboardPageCpmponent/DashboardPageComponent.jsx
+++ src/components/Dashboard/dashboardPageCpmponent/DashboardPageComponent.jsx
@@ -8,7 +8,7 @@ const DashboardPageComponent = () => {
     return (
         <>
             {/*<div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-10 m-10 text-center my-container"> */}
-        <div className="flex items-center md:items-center md:flex-row flex-col justify-between md:space-y-0 space-y-8 p-10 gap-5">
+        <div className="flex items-center md:items-center md:flex-row flex-col justify-between md:space-y-0 space-y-8 mx-6 py-10 gap-x-5">
                 <TotalUser />
 
                 <TotalPost />
@@ -39,7 +39,7 @@ const DashboardPageComponent = () => {
                     <TotalPostChart />
                 </div>
             </div>*/}
-             <div className="flex flex-col xl:flex-row items-center justify-center">
+             <div className="flex flex-col xl:flex-row items-center justify-center mx-6">
                 <div className="basis-full xl:basis-1/2">
                     <TotalUserChart />
                 </div>
