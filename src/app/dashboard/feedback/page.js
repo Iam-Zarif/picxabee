186c1d65cf946f3cc83860c3b568a60f0a950472
NISHAT TASNIM <tasnim.nt00@gmail.com>
2023-09-09 17:11:29 +0600

    fixing some issues in comment section

--- src/app/dashboard/feedback/page.js
+++ src/app/dashboard/feedback/page.js
@@ -24,49 +24,56 @@ const FeedbackPage = () => {
   };
 
   return (
-    <div className="w-10/12 mb-60 ml-auto mr-20 pt-8 mt-20 z-0">
-      {isLoading ? (
-        <>
-<div className=""><Loading /></div>
-        </>
-      ) : (
-        <>
-          <div>
-            {feedbacks?.map((feedback) => (
-              <div
-                key={feedback._id}
-                className="collapse collapse-arrow join-item border border-base-300"
-              >
-                <input type="radio" name="my-accordion-4" checked="checked" />
-                <div className="glass collapse-title text-xl font-medium">
-                  <div className="flex items-center">
-                    <Image
-                      src={feedback?.author?.profile_picture}
-                      width={50}
-                      height={50}
-                      alt="Picture of the author"
-                      className="rounded-full h-12 w-12 object-cover border p-1 mr-3"
-                    />
-                    <div>
-                      <p className="font-semibold capitalize">
-                        {feedback?.author?.name}
-                      </p>
-                      <p className="font-light text-sm">
-                        {formatDate(feedback?.createdAt)}
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <div className="collapse-content">
-                  <h1 className="px-5 py-3">{feedback?.feedback}</h1>
-                </div>
-              </div>
-            ))}
-          </div>
-        </>
-      )}
-    </div>
-  );
+		<div className="w-8/12 mb-60 ml-auto mr-40 pt-8 mt-20 z-0">
+			{isLoading ? (
+				<>
+					<div className="">
+						<Loading />
+					</div>
+				</>
+			) : (
+				<>
+					<div>
+						{feedbacks?.map((feedback) => (
+							<div
+								key={feedback._id}
+								className="collapse collapse-arrow join-item border rounded-md border-red border-opacity-20 bg-red bg-opacity-10"
+							>
+								<input
+									type="radio"
+									name="my-accordion-4"
+									checked="checked"
+									style={{ color: 'red' }}
+								/>
+								<div className="glass collapse-title text-xl font-medium">
+									<div className="flex items-center">
+										<Image
+											src={feedback?.author?.profile_picture}
+											width={50}
+											height={50}
+											alt="Picture of the author"
+											className="rounded-full h-12 w-12 object-cover border p-1 mr-3"
+										/>
+										<div>
+											<p className="font-semibold capitalize">
+												{feedback?.author?.name}
+											</p>
+											<p className="font-light text-sm">
+												{formatDate(feedback?.createdAt)}
+											</p>
+										</div>
+									</div>
+								</div>
+								<div className="collapse-content">
+									<h1 className="px-5 py-3">{feedback?.feedback}</h1>
+								</div>
+							</div>
+						))}
+					</div>
+				</>
+			)}
+		</div>
+	);
 };
 
 export default FeedbackPage;
--- src/app/dashboard/users/page.jsx
+++ src/app/dashboard/users/page.jsx
@@ -5,50 +5,52 @@ import Loading from '../activities/loading';
 import useFetchData from '@/hooks/useFetchData';
 
 const UserPage = () => {
-    
-    const { data: users, error, isLoading } = useFetchData('/api/users')
+	const { data: users, error, isLoading } = useFetchData('/api/users');
 
-    if (error) return <div>failed to load</div>;
-    if (isLoading)
-        return (
-            <div className="mx-auto  flex justify-center items-center pt-40">
-                <Loading />
-            </div>
-        );
+	if (error) return <div>failed to load</div>;
+	if (isLoading)
+		return (
+			<div className="mx-auto  flex justify-center items-center pt-40">
+				<Loading />
+			</div>
+		);
 
-    return (
-        <div className="w-10/12 ml-auto mr-28 pt-8">
-            <div className="border mb-3 w-3/12 text-2xl z-0	 p-2 rounded-md ">
-                <h1 className="">Total User: {users?.length}</h1>
-            </div>
-            <div className="w-full mb-60 ml-auto mr-28 glass mt-16 rounded-2xl z-0">
-                <div className="overflow-x-auto">
-                    <table className="table">
-                        {/* head */}
-                        <thead className="rounded-md text-base font-semibold bg-gray-400 text-gray-950 text-left">
-                            <tr>
-                                <th>*</th>
-                                <th className="w-3/12">User</th>
-                                <th className="w-3/12">email</th>
-                                <th>Joined</th>
-                                <th>Status</th>
-                                <th>Role</th>
-                                <th>Edit</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {/* row 1 */}
+	return (
+		<div className="w-10/12 ml-auto mr-24 pt-8">
+			<div className="border mb-3 w-3/12 text-2xl z-0	 p-2 rounded-md ">
+				<h1 className="">Total User: {users?.length}</h1>
+			</div>
+			<div className="w-full mb-60 ml-auto mr-24 glass mt-16 rounded-md z-0">
+				<div>
+					{/* className="overflow-x-auto" */}
+					<table className="table">
+						{/* head */}
+						<thead className="rounded-md text-base font-semibold bg-gray-400 bg-opacity-30 text-gray-950 text-left">
+							<tr>
+								<th>*</th>
+								<th className="w-3/12">User</th>
+								<th className="w-3/12">email</th>
+								<th>Joined</th>
+								<th>Status</th>
+								<th>Role</th>
+								<th>Edit</th>
+							</tr>
+						</thead>
+						<tbody>
+							{/* row 1 */}
 
-                            {users &&
-                                users.reverse().map((user, index) => (
-                                    <UserRow key={user._id} user={user} index={index}></UserRow>
-                                ))}
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
-    );
+							{users &&
+								users
+									.reverse()
+									.map((user, index) => (
+										<UserRow key={user._id} user={user} index={index}></UserRow>
+									))}
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default UserPage;
