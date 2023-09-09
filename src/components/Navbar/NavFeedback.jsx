36680cf809d77446263124ef96ad483ed511105a
Zarif <zarifraja19@gmail.com>
2023-09-10 00:16:51 +0600

    NC-45 dynamic user upgraded in navbar

--- src/components/Navbar/NavFeedback.jsx
+++ src/components/Navbar/NavFeedback.jsx
@@ -1,10 +1,13 @@
+import useLoggedInUser from '@/hooks/useLoggedInUser';
 import React from 'react'
 import { AiOutlineArrowRight, AiOutlineQuestionCircle } from 'react-icons/ai'
 
 const NavFeedback = () => {
+  const { loggedInUser } = useLoggedInUser();
+  console.log(loggedInUser?.role);
   return (
     <>
-    
+   
         <p onClick={()=>window.my_modal_1.showModal()} className=" flex items-center group  hover:ml-2 transition-all">
     <AiOutlineQuestionCircle size={28} className="inline mr-2" />
     Give Feedback
