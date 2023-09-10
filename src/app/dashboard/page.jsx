fdf4a4dfacccb774276626bfa60816def936c40c
Zarif <zarifraja19@gmail.com>
2023-09-10 21:58:31 +0600

    NC-49 just for now dashboard route is safe

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,12 +1,24 @@
+"use client"
 import UserRow from "@/components/Dashboard/Users/UserRow";
 import DashboardPageComponent from "@/components/Dashboard/dashboardPageCpmponent/DashboardPageComponent";
 import TotalUser from "@/components/Dashboard/dashboardPageCpmponent/TotalUser";
 import TotalUserChart from "@/components/Dashboard/dashboardPageCpmponent/TotalUserChart";
 import useAuth from "@/hooks/useAuth";
-import { useContext } from "react";
+import useFetchData from "@/hooks/useFetchData";
+import { useContext, useEffect } from "react";
+import { useRouter } from 'next/navigation';
 
 const DashboardPage = () => {
-	
+	// ---------------------------------------sorry to interrupt- from Zarifff---------------------------------------
+	const { user } = useAuth();
+	const { data: loggedInUser } = useFetchData(`/api/loggedInUser?userEmail=${user?.email}`);
+	const router = useRouter();
+	useEffect(() => {
+		if (loggedInUser?.role === 'user') {
+			router.push("/");
+		}
+	}, [loggedInUser?.role, router]);
+	// ---------------------------------------sorry to interrupt- from Zarifff---------------------------------------
 
 	return (
 		<>
