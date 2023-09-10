5e07ce5ae188acb16bd9673eb5721316ef844b8e
HridoyHoque <hhridoy155@gmail.com>
2023-09-10 12:48:29 +0600

    NC-47 <message>

--- src/components/HomePage/RighSidebar/Suggestions.jsx
+++ src/components/HomePage/RighSidebar/Suggestions.jsx
@@ -10,6 +10,7 @@ const Suggestions = () => {
     const SuggestedUsers = data && data?.slice(0, 6)
 
     return (
+        
         <div className="mt-4 ml-10">
             <div className="flex justify-between text-sm mb-5">
                 <h3 className="text-sm font-bold text-gray-400">Suggestions for you</h3>
