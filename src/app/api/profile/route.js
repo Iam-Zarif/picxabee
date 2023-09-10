ddde0302372f3d23fc0359ad297df9da62b05e5c
NISHAT TASNIM <tasnim.nt00@gmail.com>
2023-09-11 02:16:42 +0600

    Userposts filter

---
+++ src/app/api/profile/route.js
@@ -0,0 +1,23 @@
+import Post from '@/models/Post';
+import connect from '@/utils/db';
+import { NextResponse } from 'next/server';
+
+// Get UserPost data
+
+export const GET = async (request) => {
+	try {
+		const userEmail = request.nextUrl.searchParams.get('userEmail');
+		console.log(userEmail);
+
+		await connect();
+		const posts = await Post.find({ 'author.email': userEmail });
+		console.log(posts);
+
+		return new NextResponse(JSON.stringify(posts), { status: 200 });
+	} catch (error) {
+		console.error(error.name, error.message);
+		return new NextResponse(JSON.stringify({ error: error.message }), {
+			status: 500,
+		}); 
+	}
+};
