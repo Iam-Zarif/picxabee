f40305c56bf2f039f50394e4e602a280f1ad5db2
Jahid Howlader <jahidhowlader.info@gmail.com>
2023-09-11 20:31:15 +0600

    return backward code for faching error

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -2,7 +2,6 @@
 
 import Sidebar from '@/components/Dashboard/Sidebar/Sidebar';
 import TopBar from '@/components/Dashboard/Topbar/Topbar';
-import PrivateRoute from '@/utils/PrivateRoute';
 
 
 import React from 'react';
@@ -10,20 +9,18 @@ import React from 'react';
 const dashBoardLayout = ({ children }) => {
 	return (
 		<>
-			<PrivateRoute>
-				<div className="grid lg:grid-cols-12  ">
-					<div className="col-span-1 h-[100vh] hidden lg:block z-50">
-						<Sidebar></Sidebar>
-					</div>
-					<div className="col-span-11  z-10 ">
-						<TopBar></TopBar>
-
-						{children}
-					</div>
-
-					{/* <div className="grid col-span-10">{children}</div> */}
+			<div className="grid lg:grid-cols-12  ">
+				<div className="col-span-1 h-[100vh] hidden lg:block z-50">
+					<Sidebar></Sidebar>
 				</div>
-			</PrivateRoute>
+				<div className="col-span-11  z-10 ">
+					<TopBar></TopBar>
+
+					{children}
+				</div>
+
+				{/* <div className="grid col-span-10">{children}</div> */}
+			</div>
 		</>
 	);
 };
