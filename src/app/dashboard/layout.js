8ebe228a7a9d9f27d12984bca09f1cdf011772b0
Jahid Howlader <jahidhowlader.info@gmail.com>
2023-09-11 20:23:15 +0600

    test purpose - protect admin dashboard

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -2,6 +2,7 @@
 
 import Sidebar from '@/components/Dashboard/Sidebar/Sidebar';
 import TopBar from '@/components/Dashboard/Topbar/Topbar';
+import PrivateRoute from '@/utils/PrivateRoute';
 
 
 import React from 'react';
@@ -9,18 +10,20 @@ import React from 'react';
 const dashBoardLayout = ({ children }) => {
 	return (
 		<>
-			<div className="grid lg:grid-cols-12  ">
-				<div className="col-span-1 h-[100vh] hidden lg:block z-50">
-					<Sidebar></Sidebar>
-				</div>
-				<div className="col-span-11  z-10 ">
-					<TopBar></TopBar>
+			<PrivateRoute>
+				<div className="grid lg:grid-cols-12  ">
+					<div className="col-span-1 h-[100vh] hidden lg:block z-50">
+						<Sidebar></Sidebar>
+					</div>
+					<div className="col-span-11  z-10 ">
+						<TopBar></TopBar>
 
-					{children}
-				</div>
+						{children}
+					</div>
 
-				{/* <div className="grid col-span-10">{children}</div> */}
-			</div>
+					{/* <div className="grid col-span-10">{children}</div> */}
+				</div>
+			</PrivateRoute>
 		</>
 	);
 };
