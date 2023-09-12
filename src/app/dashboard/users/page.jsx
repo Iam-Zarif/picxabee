83a2c39060776716c2fadb38e761be7b577d8a1b
Jahid Howlader <121720284+jahidhowlader@users.noreply.github.com>
2023-09-12 20:02:15 +0600

    NC-41 complete user to admin and admin to user (#61)

--- src/app/dashboard/users/page.jsx
+++ src/app/dashboard/users/page.jsx
@@ -23,23 +23,23 @@ const UserPage = () => {
   return (
     <div className="w-10/12 ml-auto mr-24 pt-8">
       <div className="flex justify-between">
-      <div className="relative">
-        <form>
-          <InputGroup className="mb-3">
-            <Form.Control
-              onChange={(e) => setSearch(e.target.value)}
-              placeholder="User's name"
-              className="text-xl  pl-10 w-[200px] lg:w-fit mr-auto   rounded-2xl pr-2 py-2 border-1 border border-gray shadow-slate-300 hover:shadow-md hover:shadow-slate-400  focus:border-transparent focus:outline-dotted"
-            />
-          </InputGroup>
-        </form>
-        <TbUserSearch size={26} className="absolute top-2 left-2"/>
-      </div>
+        <div className="relative">
+          <form>
+            <InputGroup className="mb-3">
+              <Form.Control
+                onChange={(e) => setSearch(e.target.value)}
+                placeholder="User's name"
+                className="text-xl  pl-10 w-[200px] lg:w-fit mr-auto   rounded-2xl pr-2 py-2 border-1 border border-gray shadow-slate-300 hover:shadow-md hover:shadow-slate-400  focus:border-transparent focus:outline-dotted"
+              />
+            </InputGroup>
+          </form>
+          <TbUserSearch size={26} className="absolute top-2 left-2" />
+        </div>
         <div >
           <h1 className="border rounded-md px-4 py-2 text-xl"><span className="font-bold">Total Users </span> : {users?.length}</h1>
         </div>
       </div>
-     
+
       <div className="w-full mb-60 ml-auto mr-24 glass mt-16 rounded-md z-0">
         <div>
           {/* className="overflow-x-auto" */}
