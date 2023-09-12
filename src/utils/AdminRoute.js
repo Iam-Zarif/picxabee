f635130ba0a17adf4d353b78dce02a1e4222b5c1
Jahid Howlader <jahidhowlader.info@gmail.com>
2023-09-12 07:05:59 +0600

    test for protect admin route with vercel

--- src/utils/AdminRoute.js
+++ src/utils/AdminRoute.js
@@ -1,20 +1,39 @@
 "use client"
 
+import useAuth from '@/hooks/useAuth';
+import useFetchData from '@/hooks/useFetchData';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
 function AdminRoute({ children }) {
-  const router = useRouter();
-  const isAdmin = false; 
 
-  useEffect(() => {
-    if (!isAdmin) {
-      router.push('/'); 
+
+    const { user } = useAuth()
+
+    const { data: currentUser, isLoading } = useFetchData(`/api/loggedInUser?userEmail=${user?.email}`)
+    const router = useRouter();
+
+    // console.log('from admin route', user);
+    // console.log('from admin route', currentUser.role);
+
+    if (isLoading) {
+        return <h1>Loading...</h1>
     }
-  }, [isAdmin, router]);
-  
 
-  return isAdmin ? children : null;
+    if (currentUser?.role == 'admin') {
+        return children
+    }
+
+    // const isAdmin = true;
+
+    // useEffect(() => {
+    //     if (!isAdmin) {
+    //         router.push('/');
+    //     }
+    // }, [isAdmin, router]);
+
+
+    return router.push('/') // isAdmin ? children : null;
 }
 
 export default AdminRoute;
