3864864956543dcd13861891b532220b37055b0d
TuhinKantiPal01 <tuhinkantipal886@gmail.com>
2023-09-14 21:37:54 +0600

    NC-43 Complete Donation Api one

--- src/app/api/users/[id]/route.js
+++ src/app/api/users/[id]/route.js
@@ -1,63 +1,57 @@
-import User from '@/models/User';
-import connect from '@/utils/db';
-import { NextResponse } from 'next/server';
+import User from "@/models/User";
+import connect from "@/utils/db";
+import { NextResponse } from "next/server";
 
 export const PUT = async (request, { params }) => {
-	const { id } = params;
-	const { newFollowers } = await request.json();
+  const { id } = params;
+  const { newFollowers } = await request.json();
 
-	await connect();
+  await connect();
 
-	await User.findByIdAndUpdate(id, {
-		$push: {
-			followers: newFollowers,
-		},
-	});
-	return NextResponse.json(
-		{ message: 'Follower Successfully' },
-		{ status: 200 }
-	);
+  await User.findByIdAndUpdate(id, {
+    $push: {
+      followers: newFollowers,
+    },
+  });
+  return NextResponse.json({ message: "Follower Successfully" }, { status: 200 });
 };
 
 export const DELETE = async (request, { params }) => {
-	try {
-		const { id } = params;
-		const { email } = await request.json();
+  try {
+    const { id } = params;
+    const { email } = await request.json();
 
-		await connect();
-		const specificUser = await User.findById(id);
+    await connect();
+    const specificUser = await User.findById(id);
 
-		if (!specificUser) {
-			return NextResponse.json({ message: 'USER NOT Found' }, { status: 404 });
-		}
+    if (!specificUser) {
+      return NextResponse.json({ message: "USER NOT Found" }, { status: 404 });
+    }
 
-		specificUser.followers = specificUser.followers.filter(
-			(follower) => follower.email !== email
-		);
+    specificUser.followers = specificUser.followers.filter((follower) => follower.email !== email);
 
-		await specificUser.save();
-		return NextResponse.json(
-			{ message: 'UnFollower Successfully' },
-			{ status: 404 }
-		);
-	} catch (error) {
-		console.log(error.message);
-	}
+    await specificUser.save();
+    return NextResponse.json({ message: "UnFollower Successfully" }, { status: 404 });
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
 export const AddFollowing = async (request, { params }) => {
-	const { email } = params;
-	const { newFollowing } = await request.json();
+  
+  
+  const { newFollowing ,email} = await request.json();
+  console.log({email});
 
-	await connect();
+  const user = await User.findOne({ email });
 
-	await User.findByIdAndUpdate(email, {
-		$push: {
-			followers: newFollowers,
-		},
-	});
-	return NextResponse.json(
-		{ message: 'Follower Successfully' },
-		{ status: 200 }
-	);
+  if (!user) {
+	return NextResponse.json({Message:"User Not Found"} ,{Status:204});
+  }
+
+  user.following.push(newFollowing);
+
+  await user.save();
+  return NextResponse.json({message:"Following Successfully"})
+  
 };
