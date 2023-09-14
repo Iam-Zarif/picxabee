ac4a8175328f738249dafc6131de1706d201856c
HridoyHoque <121646317+HridoyHoque@users.noreply.github.com>
2023-09-15 01:17:15 +0600

    Nc 47 design pending donation (#104)
    * resolved approve donation page problem
    
    * NC-47 pending donation page design

--- src/components/HomePage/Donation/DonationCard.jsx
+++ src/components/HomePage/Donation/DonationCard.jsx
@@ -1,48 +1,63 @@
-"use client";
-import { checkout } from "@/hooks/checkout";
-// import Image from 'next/image';
-// import Image from 'next/image';
-const DonationCard = ({ title, imageUrl, price }) => {
-  return (
-    <div className="mb-2">
-      <div className="card w-full glass rounded-md">
-        <div className="ml-8 flex items-center space-x-4 py-4">
-          <div className="avatar">
-            <div className="rounded-full w-12 h-12">
-              <img
-                src="https://i.ibb.co/P5ghfdx/admin.jpg"
-                alt="avatar"
-              />
+import React from 'react';
+import { checkout } from '@/hooks/checkout';
+import Image from 'next/image';
+
+const DonationCard = ({ title, imageUrl, totalDonated, userProfileImage, username }) => {
+    return (
+        <div className='mb-2'>
+            <div className="card w-80 glass rounded-md">
+                <Image
+                    src={imageUrl}
+                    alt="donate"
+                    layout="responsive"
+                    width={400}
+                    height={300}
+                    className="card-image"
+                />
+                <div className="card-body">
+                    <h2 className="card-title">{title}</h2>
+                    
+                    {/* Display user profile picture and username */}
+                    <div className="flex items-center mt-3">
+                        <div className="w-8 h-8 rounded-full mr-2 overflow-hidden">
+                            <Image
+                                src={userProfileImage}
+                                alt="User Profile"
+                                layout="responsive"
+                                width={32}
+                                height={32}
+                            />
+                        </div>
+                        <span className="text-sm font-semibold">{username}</span>
+                    </div>
+                    
+                    <div className="card-actions flex mt-3">
+                        <div className='flex'>
+                            <div className="card-action">
+                                <button
+                                    className="btn rounded-md hover:bg-primary-color hover:text-white border-gray font-semibold lg:ml-5 capitalize dark:border-white dark:hover:bg-black"
+                                >
+                                    Total Donated {totalDonated}$
+                                </button>
+                            </div>
+                            <div className="card-action">
+                                <button
+                                    onClick={() => {
+                                        checkout({
+                                            lineItems: [{ price: "price_1Nn2TMIfrNNr5g42vzeaIzwf", quantity: 1 }]
+                                        });
+                                    }}
+                                    className="btn rounded-md hover:bg-primary-color hover:text-white border-gray font-semibold lg:ml-5 capitalize dark:border-white dark:hover:bg-black"
+                                >
+                                    Donate now
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-          <div>
-            <div className="font-bold">Brice Swyre</div>
-          </div>
         </div>
-        {/* ...................................... */}
-        <img src={imageUrl} alt="donate" />
-        <div className="card-body">
-          <h2 className="card-title">Total donated {price}$</h2>
-          <p>{title}</p>
-          <div className="card-actions justify-end">
-            <button
-              onClick={() => {
-                checkout({
-                  lineItems: [
-                    { price: "price_1Nn2TMIfrNNr5g42vzeaIzwf", quantity: 1 },
-                  ],
-                });
-              }}
-              className="btn
-                    rounded-md hover:bg-primary-color hover:text-white border-gray font-semibold lg:ml-5 capitalize dark:border-white dark:hover:bg-black"
-            >
-              Donate now
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default DonationCard;
--- src/components/HomePage/Donation/DonationCards.jsx
+++ src/components/HomePage/Donation/DonationCards.jsx
@@ -1,42 +1,47 @@
-"use client";
+"use client"
 
-import useFetchData from "@/hooks/useFetchData";
-import { useRouter } from "next/navigation";
-import DonationCard from "./DonationCard";
+import React from 'react';
+import DonationCard from './DonationCard';
+import useSWR from 'swr';
 
-const DonationCards = () => {  
-	const router = useRouter();
-  // const{ data } =useFetchData(`/api/donation`)
-  // console.log(data)
+const DonationCards = () => {
 
-  const { data } = useFetchData("/api/donation?currentStatus=approved");
-  console.log(data);
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const { data: donationPosts, error, isLoading } = useSWR('/api/donation', fetcher, {
+    refreshInterval: 1000,
+  });
 
-	const donations = [
-		{
-			title: 'Project A',
-			imageUrl: 'https://i.ibb.co/z81C6nJ/donate1.png',
-			price: 10,
-		},
-		{
-			title: 'Project B',
-			imageUrl: 'https://i.ibb.co/FqSRmnw/donatess.png',
-			price: 20,
-		},
-		// Add more donation items as needed
-	];
-	return (
-		<div className="">
-			{donations.map((donation, index) => (
-				<DonationCard
-					key={index}
-					title={donation.title}
-					imageUrl={donation.imageUrl}
-					price={donation.price}
-				/>
-			))}
-		</div>
-	);
+  // Checking if there's an error or if the data is still loading
+  if (error) return <div>Failed to load</div>;
+  if (isLoading) return <div>Loading...</div>;
+
+  // Filter the donation posts based on criteria
+
+  const approvedDonations = donationPosts && donationPosts?.filter(donationPost => donationPost.status === 'approved');
+  const displayDonation = approvedDonations.slice(0, 2);
+
+  console.log(displayDonation)
+  return (
+
+    <div className="p-4">
+      {
+
+        displayDonation.map((donation, index) => (
+          <DonationCard
+            key={index}
+            title={donation?.content}
+            imageUrl={donation?.image}
+            totalDonated={donation?.amount}
+            userProfileImage={donation?.author?.profile_picture}
+            username={donation?.author?.name}
+
+          />
+        ))
+
+      }
+    </div>
+
+  );
 };
 
 export default DonationCards;
