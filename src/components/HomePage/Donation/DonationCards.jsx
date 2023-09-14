04625dbbb64cd096f360c1df4ffb81cb722f40b4
Jahid Howlader <121720284+jahidhowlader@users.noreply.github.com>
2023-09-15 01:12:57 +0600

    Nc 63 dynamic user profile (#103)
    * NC-41 complete user to admin and admin to user
    
    * NC-39 complete recycle feature
    
    * NC-58 complete story feature
    
    * try to fixed bug
    
    * NC-42 add feature password reset
    
    * NC-42 polish password reset page
    
    * NC-63 create dynamic uer profile api
    
    * NC-63 fixed bug Dynamic user profile API
    
    * NC-63 fetch dynamic post user dynamic  page

--- src/components/HomePage/Donation/DonationCards.jsx
+++ src/components/HomePage/Donation/DonationCards.jsx
@@ -12,40 +12,31 @@ const DonationCards = () => {
   const { data } = useFetchData("/api/donation?currentStatus=approved");
   console.log(data);
 
-  const donations = [
-    {
-      title: "Project A",
-      imageUrl: "https://i.ibb.co/z81C6nJ/donate1.png",
-      price: 10,
-    },
-    {
-      title: "Project B",
-      imageUrl: "https://i.ibb.co/FqSRmnw/donatess.png",
-      price: 20,
-    },
-    // Add more donation items as needed
-  ];
-  return (
-    <div className="flex flex-col items-center justify-between">
-      <div className="p-4">
-        {donations.map((donation, index) => (
-          <DonationCard
-            key={index}
-            title={donation.title}
-            imageUrl={donation.imageUrl}
-            price={donation.price}
-          />
-        ))}
-      </div>
-      <button
-        className="btn
-                    rounded-md hover:bg-primary-color hover:text-white border-gray font-semibold lg:ml-5 capitalize dark:border-white dark:hover:bg-black"
-					onClick={()=> router.push("/AllDonations")}
-      >
-        See more
-      </button>
-    </div>
-  );
+	const donations = [
+		{
+			title: 'Project A',
+			imageUrl: 'https://i.ibb.co/z81C6nJ/donate1.png',
+			price: 10,
+		},
+		{
+			title: 'Project B',
+			imageUrl: 'https://i.ibb.co/FqSRmnw/donatess.png',
+			price: 20,
+		},
+		// Add more donation items as needed
+	];
+	return (
+		<div className="">
+			{donations.map((donation, index) => (
+				<DonationCard
+					key={index}
+					title={donation.title}
+					imageUrl={donation.imageUrl}
+					price={donation.price}
+				/>
+			))}
+		</div>
+	);
 };
 
 export default DonationCards;
