16ae5817b89d53adbd7f7ca0418f38c657a77b17
Rezwan Faysal <rezwanfaysalrezon@gmail.com>
2023-09-14 20:48:24 +0600

    added donation cards in different page

--- src/components/HomePage/Donation/DonationCards.jsx
+++ src/components/HomePage/Donation/DonationCards.jsx
@@ -1,41 +1,51 @@
 "use client";
 
-import React, { useState } from "react";
-import DonationCard from "./DonationCard";
 import useFetchData from "@/hooks/useFetchData";
+import { useRouter } from "next/navigation";
+import DonationCard from "./DonationCard";
 
-const DonationCards = () => {
-	// const{ data } =useFetchData(`/api/donation`)
-	// console.log(data)
+const DonationCards = () => {  
+	const router = useRouter();
+  // const{ data } =useFetchData(`/api/donation`)
+  // console.log(data)
 
-	const { data } = useFetchData("/api/donation?currentStatus=approved");
-	console.log(data)
+  const { data } = useFetchData("/api/donation?currentStatus=approved");
+  console.log(data);
 
-	const donations = [
-		{
-			title: 'Project A',
-			imageUrl: 'https://i.ibb.co/z81C6nJ/donate1.png',
-			price: 10,
-		},
-		{
-			title: 'Project B',
-			imageUrl: 'https://i.ibb.co/FqSRmnw/donatess.png',
-			price: 20,
-		},
-		// Add more donation items as needed
-	];
-	return (
-		<div className="p-4">
-			{donations.map((donation, index) => (
-				<DonationCard
-					key={index}
-					title={donation.title}
-					imageUrl={donation.imageUrl}
-					price={donation.price}
-				/>
-			))}
-		</div>
-	);
+  const donations = [
+    {
+      title: "Project A",
+      imageUrl: "https://i.ibb.co/z81C6nJ/donate1.png",
+      price: 10,
+    },
+    {
+      title: "Project B",
+      imageUrl: "https://i.ibb.co/FqSRmnw/donatess.png",
+      price: 20,
+    },
+    // Add more donation items as needed
+  ];
+  return (
+    <div className="flex flex-col items-center justify-between">
+      <div className="p-4">
+        {donations.map((donation, index) => (
+          <DonationCard
+            key={index}
+            title={donation.title}
+            imageUrl={donation.imageUrl}
+            price={donation.price}
+          />
+        ))}
+      </div>
+      <button
+        className="btn
+                    rounded-md hover:bg-primary-color hover:text-white border-gray font-semibold lg:ml-5 capitalize dark:border-white dark:hover:bg-black"
+					onClick={()=> router.push("/AllDonations")}
+      >
+        See more
+      </button>
+    </div>
+  );
 };
 
 export default DonationCards;
