e6c30e6ce14bcd028070e9d6a6d7d15de251c418
Jahid Howlader <jahidhowlader.info@gmail.com>
2023-09-15 03:01:20 +0600

    try to fixed bug on application

--- src/components/MessagesComponents/MessagesSidebarLeft/ChatChats.jsx
+++ src/components/MessagesComponents/MessagesSidebarLeft/ChatChats.jsx
@@ -29,27 +29,27 @@ const ChatChats = () => {
   const handleSelect = (u) => {
     dispatch({ type: "CHANGE_USER", payload: u })
 
-    if (window.innerWidth <= 768) {
-      // Screen width less than or equal to 768px (adjust as needed)
-      setDrawerOn(false);
-    } else {
-      setDrawerOn(true);
-    }
+    // if (window.innerWidth <= 768) {
+    //   // Screen width less than or equal to 768px (adjust as needed)
+    //   setDrawerOn(false);
+    // } else {
+    //   setDrawerOn(true);
+    // }
   }
 
   return (
     <div>
-      {chats && (Object.entries(chats)?.sort((a,b)=> b[1].date - a[1].date)?.map(chat => (
-          <div key={chat[0]} className='userChat text-white cursor-pointer md:text-base text-sm m-2' onClick={()=> handleSelect(chat[1]?.userInfo)}>
+      {chats && (Object.entries(chats)?.sort((a, b) => b[1].date - a[1].date)?.map(chat => (
+        <div key={chat[0]} className='userChat text-white cursor-pointer md:text-base text-sm m-2' onClick={() => handleSelect(chat[1]?.userInfo)}>
           <div className="hover:bg-primary-color rounded-md px-2 h-20 w-full flex items-center gap-3 transition-all ease-in-out">
-              <img className="w-14 h-14 object-cover rounded-full" src={chat[1].userInfo.photoURL} alt="" />
-              <div className='userChatInfo  flex-1'>
-                  <p className='font-bold text-black'>{chat[1].userInfo.displayName}</p>
-                  <span className='md:text-sm text-xs text-black'>{chat[1].lastMessage?.text}</span>
-              </div>
+            <img className="w-14 h-14 object-cover rounded-full" src={chat[1].userInfo.photoURL} alt="" />
+            <div className='userChatInfo  flex-1'>
+              <p className='font-bold text-black'>{chat[1].userInfo.displayName}</p>
+              <span className='md:text-sm text-xs text-black'>{chat[1].lastMessage?.text}</span>
+            </div>
           </div>
-      </div>
-        )))}
+        </div>
+      )))}
     </div>
   );
 };
