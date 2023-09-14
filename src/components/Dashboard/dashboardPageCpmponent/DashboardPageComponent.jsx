50b566c20fea507c159599ace25458f4cebdf508
Zarif <zarifraja19@gmail.com>
2023-09-14 12:26:25 +0600

    NC-61 not completed yet the responsive just fixed the extra padding on the head

--- src/components/Dashboard/dashboardPageCpmponent/DashboardPageComponent.jsx
+++ src/components/Dashboard/dashboardPageCpmponent/DashboardPageComponent.jsx
@@ -11,12 +11,13 @@ const DashboardPageComponent = () => {
   return (
     <>
       {/*<div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-10 m-10 text-center my-container"> */}
-      <div className="flex items-center md:items-center md:flex-row flex-col justify-between md:space-y-0 space-y-8 mx-6 py-10 gap-x-5">
+      <div className="flex items-center   justify-between  mx-6 py-10 gap-x-5">
         <TotalUser />
 
         <TotalPost />
         <TotalFeedback />
 
+        </div>
         {/* <TotalUser /> */}
 
         {/* <div className="bg-red bg-opacity-10 h-[150px] rounded-md flex items-center justify-center">
@@ -26,7 +27,6 @@ const DashboardPageComponent = () => {
                 <div className="bg-blue bg-opacity-30 h-[150px] rounded-md flex items-center justify-center">
                     <h3 className="text-2xl font-semibold">Total User: 28</h3>
                 </div>*/}
-      </div>
 
       {/* <div className="grid grid-cols-1 md:grid-cols-6 gap-x-10 m-10">
                 <div className="col-span-1 md:col-span-3 w-full">
@@ -36,14 +36,14 @@ const DashboardPageComponent = () => {
                     <TotalPostChart />
                 </div>
             </div>*/}
-			<div className="flex flex-col xl:flex-row gap-5 items-center justify-center mx-6">
+			{/* <div className="flex flex-col xl:flex-row gap-5 items-center justify-center mx-6">
 				<div className="basis-full xl:basis-1/2">
 					<TotalUserChart />
 				</div>
 				<div className="basis-full xl:basis-1/2">
 					<TotalPostChart />
 				</div>
-			</div>
+			</div> */}
 		</>
 	);
 };
