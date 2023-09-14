7cd351af6611bd8e9834618dd5529a6fc6376529
Jahid Howlader <jahidhowlader.info@gmail.com>
2023-09-15 02:43:37 +0600

    try to fixed bug on application

--- src/components/HomePage/Donation/DonationCard.jsx
+++ src/components/HomePage/Donation/DonationCard.jsx
@@ -1,63 +1,63 @@
-import React from 'react';
-import { checkout } from '@/hooks/checkout';
-import Image from 'next/image';
+// import React from 'react';
+// import { checkout } from '@/hooks/checkout';
+// import Image from 'next/image';
 
-const DonationCard = ({ title, imageUrl, totalDonated, userProfileImage, username }) => {
-    return (
-        <div className='mb-2'>
-            <div className="card w-full glass rounded-md">
-                <Image
-                    src={imageUrl}
-                    alt="donate"
-                    layout="responsive"
-                    width={400}
-                    height={300}
-                    className="card-image"
-                />
-                <div className="card-body">
-                    <h2 className="card-title">{title}</h2>
+// const DonationCard = ({ title, imageUrl, totalDonated, userProfileImage, username }) => {
+//     return (
+//         <div className='mb-2'>
+//             <div className="card w-full glass rounded-md">
+//                 <Image
+//                     src={imageUrl}
+//                     alt="donate"
+//                     layout="responsive"
+//                     width={400}
+//                     height={300}
+//                     className="card-image"
+//                 />
+//                 <div className="card-body">
+//                     <h2 className="card-title">{title}</h2>
                     
-                    {/* Display user profile picture and username */}
-                    <div className="flex items-center mt-3">
-                        <div className="w-8 h-8 rounded-full mr-2 overflow-hidden">
-                            <Image
-                                src={userProfileImage}
-                                alt="User Profile"
-                                layout="responsive"
-                                width={32}
-                                height={32}
-                            />
-                        </div>
-                        <span className="text-sm font-semibold">{username}</span>
-                    </div>
+//                     {/* Display user profile picture and username */}
+//                     <div className="flex items-center mt-3">
+//                         <div className="w-8 h-8 rounded-full mr-2 overflow-hidden">
+//                             <Image
+//                                 src={userProfileImage}
+//                                 alt="User Profile"
+//                                 layout="responsive"
+//                                 width={32}
+//                                 height={32}
+//                             />
+//                         </div>
+//                         <span className="text-sm font-semibold">{username}</span>
+//                     </div>
                     
-                    <div className="card-actions flex mt-3">
-                        <div className='flex'>
-                            <div className="card-action">
-                                <button
-                                    className="btn rounded-md hover:bg-primary-color hover:text-white border-gray font-semibold lg:ml-5 capitalize dark:border-white dark:hover:bg-black"
-                                >
-                                    Total Donated {totalDonated}$
-                                </button>
-                            </div>
-                            <div className="card-action">
-                                <button
-                                    onClick={() => {
-                                        checkout({
-                                            lineItems: [{ price: "price_1Nn2TMIfrNNr5g42vzeaIzwf", quantity: 1 }]
-                                        });
-                                    }}
-                                    className="btn rounded-md hover:bg-primary-color hover:text-white border-gray font-semibold lg:ml-5 capitalize dark:border-white dark:hover:bg-black"
-                                >
-                                    Donate now
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
-};
+//                     <div className="card-actions flex mt-3">
+//                         <div className='flex'>
+//                             <div className="card-action">
+//                                 <button
+//                                     className="btn rounded-md hover:bg-primary-color hover:text-white border-gray font-semibold lg:ml-5 capitalize dark:border-white dark:hover:bg-black"
+//                                 >
+//                                     Total Donated {totalDonated}$
+//                                 </button>
+//                             </div>
+//                             <div className="card-action">
+//                                 <button
+//                                     onClick={() => {
+//                                         checkout({
+//                                             lineItems: [{ price: "price_1Nn2TMIfrNNr5g42vzeaIzwf", quantity: 1 }]
+//                                         });
+//                                     }}
+//                                     className="btn rounded-md hover:bg-primary-color hover:text-white border-gray font-semibold lg:ml-5 capitalize dark:border-white dark:hover:bg-black"
+//                                 >
+//                                     Donate now
+//                                 </button>
+//                             </div>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// };
 
-export default DonationCard;
+// export default DonationCard;
--- src/components/HomePage/Donation/DonationCards.jsx
+++ src/components/HomePage/Donation/DonationCards.jsx
@@ -1,53 +1,53 @@
-"use client"
+// "use client"
 
-import React from 'react';
-import DonationCard from './DonationCard';
-import useSWR from 'swr';
-import { useRouter } from 'next/navigation';
-import useFetchData from '@/hooks/useFetchData';
+// import React from 'react';
+// import DonationCard from './DonationCard';
+// import useSWR from 'swr';
+// import { useRouter } from 'next/navigation';
+// import useFetchData from '@/hooks/useFetchData';
 
-const DonationCards = () => {
-  const router = useRouter();
-  // const{ data } =useFetchData(`/api/donation`)
-  // console.log(data)
+// const DonationCards = () => {
+//   const router = useRouter();
+//   // const{ data } =useFetchData(`/api/donation`)
+//   // console.log(data)
 
-  const { data } = useFetchData("/api/donation?currentStatus=approved");
-  console.log(data);
+//   const { data } = useFetchData("/api/donation?currentStatus=approved");
+//   console.log(data);
 
-  const donations = [
-    {
-      title: "Project A",
-      imageUrl: "https://i.ibb.co/z81C6nJ/donate1.png",
-      price: 10,
-    },
-    {
-      title: "Project B",
-      imageUrl: "https://i.ibb.co/FqSRmnw/donatess.png",
-      price: 20,
-    },
-    // Add more donation items as needed
-  ];
-  return (
-    <div className="flex flex-col items-center justify-between">
-      <div className="">
-        {donations.map((donation, index) => (
-          <DonationCard
-            key={index}
-            title={donation.title}
-            imageUrl={donation.imageUrl}
-            price={donation.price}
-          />
-        ))}
-      </div>
-      <button
-        className="btn
-                    rounded-md hover:bg-primary-color hover:text-white border-gray font-semibold lg:ml-5 capitalize dark:border-white dark:hover:bg-black"
-        onClick={() => router.push("/AllDonations")}
-      >
-        See more
-      </button>
-    </div>
-  );
-};
+//   const donations = [
+//     {
+//       title: "Project A",
+//       imageUrl: "https://i.ibb.co/z81C6nJ/donate1.png",
+//       price: 10,
+//     },
+//     {
+//       title: "Project B",
+//       imageUrl: "https://i.ibb.co/FqSRmnw/donatess.png",
+//       price: 20,
+//     },
+//     // Add more donation items as needed
+//   ];
+//   return (
+//     <div className="flex flex-col items-center justify-between">
+//       <div className="">
+//         {donations.map((donation, index) => (
+//           <DonationCard
+//             key={index}
+//             title={donation.title}
+//             imageUrl={donation.imageUrl}
+//             price={donation.price}
+//           />
+//         ))}
+//       </div>
+//       <button
+//         className="btn
+//                     rounded-md hover:bg-primary-color hover:text-white border-gray font-semibold lg:ml-5 capitalize dark:border-white dark:hover:bg-black"
+//         onClick={() => router.push("/AllDonations")}
+//       >
+//         See more
+//       </button>
+//     </div>
+//   );
+// };
 
-export default DonationCards;
+// export default DonationCards;
