ccbdbf6eac6b20fba78d9b92214e2cc87ac838f3
Zarif <zarifraja19@gmail.com>
2023-09-15 21:02:08 +0600

    added

--- src/components/HomePage/Donation/DonationCards.jsx
+++ src/components/HomePage/Donation/DonationCards.jsx
@@ -1,5 +1,5 @@
 "use client"
-
+import Link from "next/link"
 import React from 'react';
 import DonationCard from './DonationCard';
 import useSWR from 'swr';
@@ -22,7 +22,7 @@ const DonationCards = () => {
 
   console.log(displayDonation)
   return (
-
+<>
     <div className="">
       {
 
@@ -40,6 +40,12 @@ const DonationCards = () => {
 
       }
     </div>
+    <Link href="/allApprovedDonation">   <button
+        className="btn rounded-md hover:bg-primary-color hover:text-white border-gray font-semibold lg:ml-5 capitalize dark:border-white dark:hover:bg-black"
+      >
+        See more
+      </button></Link>
+    </>
 
   );
 };
