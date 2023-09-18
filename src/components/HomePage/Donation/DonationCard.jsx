d5cbfb7ae29ae1a4613f91aaf0d6e8a24c534724
HridoyHoque <hhridoy155@gmail.com>
2023-09-19 02:14:09 +0600

    NC-47 changed own profile design

--- src/components/HomePage/Donation/DonationCard.jsx
+++ src/components/HomePage/Donation/DonationCard.jsx
@@ -6,17 +6,10 @@ const DonationCard = ({ title, imageUrl, totalDonated, userProfileImage, usernam
 
     const handlePaymentSuccess = async () => {
         try {
-            console.log('Button clicked');
             const paymentResponse = await checkout({
                 lineItems: [{ price: "price_1Nn2TMIfrNNr5g42vzeaIzwf", quantity: 1 }],
             });
-            console.log('success brooo');
-            // Check if payment was successful based on Stripe's response
-            if (paymentResponse) {
-                console.log('Payment successful!');
-            } else {
-                console.log('Payment successful!');
-            }
+      
         } catch (error) {
             console.error('Payment error:', error);
         }
@@ -51,19 +44,19 @@ const DonationCard = ({ title, imageUrl, totalDonated, userProfileImage, usernam
                     </div>
 
                     <div className="card-actions flex mt-3">
-                        <div className='flex'>
+                        <div>
                             <div className="card-action">
-                                <button
+                                {/* <button
                                     className="btn rounded-md hover:bg-primary-color hover:text-white border-gray font-semibold lg:ml-5 capitalize dark:border-white dark:hover:bg-black"
                                 >
                                     Total Donated {totalDonated}$
-                                </button>
+                                </button> */}
                             </div>
                             <div className="card-action">
                                 <button
 
                                     onClick={handlePaymentSuccess}
-                                    className="btn rounded-md hover:bg-primary-color hover:text-white border-gray font-semibold lg:ml-5 capitalize dark:border-white dark:hover:bg-black"
+                                    className="btn rounded-md hover:bg-primary-color hover:text-white border-gray font-semibold lg:ml-5 capitalize dark:border-white dark:hover:bg-black mx-auto"
                                 >
                                     Donate now
                                 </button>
--- src/components/OwnProfile/EditProfileModal.jsx
+++ src/components/OwnProfile/EditProfileModal.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 
-const EditProfileModal = ({isVisible}) => {
+const EditProfileModal = ({ isVisible, onClose, children }) => {
 
-    if(!isVisible) return null;
-    return (
-        <div className='fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center'>
-          <div className='w-[600px] flex flex-col'>
-            <button className='text-white text-xl place-self-end'>X</button>
-            <div className='bg-white p-2 rounded'>Modal</div>
-          </div>
-        </div>
-    );
+  if (!isVisible) return null;
+
+  const handleClose = (e) => {
+    if (e.target.id === "wrapper") onClose ();
+    }
+  return (
+    <div className='fixed inset-0 bg-opacity-25 backdrop-blur-sm flex justify-center items-center' id='wrapper' onClick={() => onClose(handleClose)}>
+      <div className='w-[600px] flex flex-col'>
+        <button
+          onClick={() => onClose()}
+          className='text-white text-xl place-self-end'>X</button>
+        <div className='bg-white p-2 rounded'>{children}</div>
+      </div>
+    </div>
+  );
 };
 
 export default EditProfileModal;
