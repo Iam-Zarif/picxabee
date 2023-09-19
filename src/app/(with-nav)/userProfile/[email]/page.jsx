7782745e5c9c4043b383b705ecd86979d4baf752
HridoyHoque <hhridoy155@gmail.com>
2023-09-19 21:17:49 +0600

    added react hook form in the modal

--- src/app/(with-nav)/userProfile/[email]/page.jsx
+++ src/app/(with-nav)/userProfile/[email]/page.jsx
@@ -3,15 +3,17 @@ import SinglePost from '@/components/HomePage/Feed/postCard/SinglePost';
 import Navbar from '@/components/Navbar/Navbar';
 import useFetchData from '@/hooks/useFetchData';
 import Image from 'next/image';
-import React from 'react';
+import React, { useState } from 'react';
 import { FaFacebookSquare, FaGraduationCap, FaInstagramSquare, FaLinkedin, FaSchool } from 'react-icons/fa';
 import { HiMiniNoSymbol, HiMiniPencilSquare } from 'react-icons/hi2';
 import useSWR from 'swr'
 import styles from '../userprofile.module.css'
 import useAuth from '@/hooks/useAuth';
+import EditProfileModal from '@/components/OwnProfile/EditProfileModal';
+import { useForm } from 'react-hook-form';
 
 const UserProfile = ({ params }) => {
-
+    const { register, handleSubmit } = useForm();
     const { user } = useAuth()
     const email = params.email.replace('%40', '@')
 
@@ -23,6 +25,15 @@ const UserProfile = ({ params }) => {
     const { data: ownPosts, error, isLoading, } = useSWR(`/api/profile?userEmail=${data?.singleUser?.email}`, fetcher);
 
     console.log(ownPosts);
+
+    const [showModal, setShowModal] = useState(false);
+
+      const onSubmit = (data) => {
+        console.log(data.username)
+        console.log(data.bio)
+        console.log(data.college)
+        console.log(data.school)
+    }
 
     return (
         <>
@@ -97,7 +108,7 @@ const UserProfile = ({ params }) => {
                                 user?.email !== email ?
                                     <></> :
                                     <div className='mt-3'>
-                                        <HiMiniPencilSquare size={20} />
+                                       <button onClick={() => setShowModal(true)}> <HiMiniPencilSquare size={20} /></button>
                                     </div>
                             }
                         </div>
@@ -131,6 +142,62 @@ const UserProfile = ({ params }) => {
                     </div>
                 </div>
             </div>
+
+            <EditProfileModal isVisible={showModal} onClose={() => setShowModal(false)}>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className='bg-white p-2 rounded'>
+                        <div className='mb-4'>
+                            <label className='block text-sm font-medium text-gray-700'>Username</label>
+                            <input
+                                {...register('username')}
+                                type='text'
+                                name='username'
+                                // defaultValue={loggedInUser?.name}
+
+                                className='w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-primary-color'
+                            />
+                        </div>
+                        <div className='mb-4'>
+                            <label className='block text-sm font-medium text-gray-700'>Bio</label>
+                            <textarea
+                                {...register('bio')}
+                                name='bio'
+                                // defaultValue={loggedInUser?.bio}
+                                rows={3}
+                                className='w-full px-3 py-2 border border-gray-300 rounded-md resize-none focus:outline-none focus:border-primary-color'
+                            />
+                        </div>
+                        <div className='mb-4'>
+                            <label className='block text-sm font-medium text-gray-700'>College Name</label>
+                            <input
+                                {...register('college')}
+                                type='text'
+                                name='college'
+                                // defaultValue={loggedInUser?.information?.college}
+
+                                className='w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-primary-color'
+                            />
+                        </div>
+                        <div className='mb-4'>
+                            <label className='block text-sm font-medium text-gray-700'>School Name</label>
+                            <input
+                                {...register('school')}
+                                type='text'
+                                name='school'
+                                // defaultValue={loggedInUser?.information?.school}
+
+                                className='w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-primary-color'
+                            />
+                        </div>
+                        <button
+                            type='submit'
+                            className='px-4 py-2 text-white bg-primary-color rounded-md hover:bg-primary-color-dark focus:outline-none focus:ring'
+                        >
+                            Save Changes
+                        </button>
+                    </div>
+                </form>
+            </EditProfileModal>  
         </>
     );
 };
