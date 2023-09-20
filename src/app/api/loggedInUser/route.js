22c042da8380591537518cba7268802549f8b00f
TuhinKantiPal01 <tuhinkantipal886@gmail.com>
2023-09-21 02:14:12 +0600

    NC-76 <cover and profile>

--- src/app/api/loggedInUser/route.js
+++ src/app/api/loggedInUser/route.js
@@ -14,38 +14,33 @@ export const GET = async (request) => {
   }
 };
 
-// // Update LoggedIn UserInformation
-// export const PUT = async (request) => {
-//   try {
-//     // const { newProfileInfo } = await request.json();
-//     const userEmail = request.nextUrl.searchParams.get("userEmail");
-//     await connect();
-//     const user = await User.findByID();
-// 	console.log(user);
-//     user.information = {
-//       school: "No School",
-//       college: "NAI",
-//       university: "NAI",
-//       location: "NAI",
-//       gender: "NAI",
-//     };
-//     await user.save();
-
 // Update LoggedIn UserInformation
 // Update LoggedIn UserInformation
 export const PUT = async (request) => {
-	try {
-		const id = request.nextUrl.searchParams.get('id');
-		const newProfileInfo = await request.json();
-		const { name, bio, information } = newProfileInfo;
-		await connect();
-		await User.findByIdAndUpdate(id, { name, bio, information });
-		return NextResponse.json(
-			{ message: 'user information updated' },
-			{ status: 200 }
-		);
-	} catch (error) {
-		console.log(error.name, error.message);
-		return NextResponse.json({ error: error.message });
-	}
+  try {
+    const id = request.nextUrl.searchParams.get("id");
+    const newProfileInfo = await request.json();
+
+    // const coverPhoto
+    const { cover_photo } = newProfileInfo;
+    const { profile_picture } = newProfileInfo;
+
+    if (cover_photo) {
+      await connect();
+      await User.findByIdAndUpdate(id, { cover_photo });
+      return NextResponse.json({ message: "Cover Image Success" }, { status: 200 });
+    } else if (profile_picture) {
+      await connect();
+      await User.findByIdAndUpdate(id, { profile_picture });
+      return NextResponse.json({ message: "Profile Image Success" }, { status: 200 });
+    } else {
+      const { name, bio, information } = newProfileInfo;
+      await connect();
+      await User.findByIdAndUpdate(id, { name, bio, information });
+      return NextResponse.json({ message: "user information updated" }, { status: 200 });
+    }
+  } catch (error) {
+    console.log(error.name, error.message);
+    return NextResponse.json({ error: error.message });
+  }
 };
