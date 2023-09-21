82325afc44dddb65b4123bce10130ccf04c467c6
HridoyHoque <hhridoy155@gmail.com>
2023-09-21 09:22:04 +0600

    payment schema

--- src/app/(with-nav)/confirmDonation/page.jsx
+++ src/app/(with-nav)/confirmDonation/page.jsx
@@ -1,43 +1,54 @@
 "use client"
 import Navbar from '@/components/Navbar/Navbar';
 import EditProfileModal from '@/components/OwnProfile/EditProfileModal';
+import useAuth from '@/hooks/useAuth';
 import Image from 'next/image';
 import React, { useState } from 'react';
 import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
 
 const ConfirmationPage = () => {
-  
+   
     const imageUrl = 'https://i.ibb.co/X2q6ckZ/confirm-donation.png';
-
+    const { user } = useAuth()
     const { register, handleSubmit } = useForm();
     const [showModal, setShowModal] = useState(false);
-    const onSubmit = (userData) => {
-        setShowModal(false);
-        const additionalInfo = {
-         
+    const onSubmit = async (data) => {
+        
+        const payments = {
+            author: {
+                profile_picture: user?.photoURL,
+                email: user?.email,
+                name: user?.displayName,
+              },
+            email: data?.email,
+            address: data?.address,
+            number: data?.number,
+            amount: 50
         };
-    
-        // fetch(`/api/loggedInUser?id=${id}`, {
-        //   method: "PUT",
-        //   headers: {
-        //     "content-type": "application/json",
-        //   },
-        //   body: JSON.stringify(newProfileInfo),
-        // })
-        //   .then((res) => {
-        //     if (!res.ok) {
-        //       throw new Error("Network response was not ok");
-        //     }
-        //     // Parse and return the JSON response
-        //     return res.json();
-        //   })
-        //   .then((data) => {
-        //     console.log("Received data:", data);
-        //   })
-        //   .catch((error) => {
-        //     console.error("Fetch error:", error);
-        //   });
-      };
+        console.log(payments)
+        try {
+            const res = await fetch("api/payments", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(payments),
+            });
+      
+            if (res.ok) {
+              
+            } else {
+              console.error("Error submitting feedback.");
+      
+            }
+            setShowModal(false)
+            toast.success("Payment Success");
+          } catch (error) {
+            console.error("An error occurred:", error);
+            
+          }
+    };
 
     return (
         <>
@@ -48,8 +59,8 @@ const ConfirmationPage = () => {
                 <Image
                     src={imageUrl}
                     alt="Confirmation"
-                    width={400} 
-                    height={300} 
+                    width={400}
+                    height={300}
                     className="max-w-md mx-auto mb-4"
                 />
 
@@ -68,50 +79,51 @@ const ConfirmationPage = () => {
                 >
                     Confirm Donation
                 </button>
-                
-            {/* Modal */}
-            <EditProfileModal isVisible={showModal} onClose={() => setShowModal(false)}>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <div className="bg-white p-2 rounded">
-                        <div className="mb-4">
-                            <label className="block text-sm font-medium text-gray-700">Email</label>
-                            <input
-                                {...register("email")}
-                                type="text"
-                        
-                                name="email"
-                                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-primary-color"
-                            />
+
+                {/* Modal */}
+                <EditProfileModal isVisible={showModal} onClose={() => setShowModal(false)}>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div className="bg-white p-2 rounded">
+                            <div className="mb-4">
+                                <label className="block text-sm font-medium text-gray-700">Email</label>
+                                <input
+                                    {...register("email")}
+                                    type="text"
+                                    defaultValue={user?.email}
+                                    name="email"
+                                    readOnly
+                                    className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-primary-color"
+                                />
+                            </div>
+                            <div className="mb-4">
+                                <label className="block text-sm font-medium text-gray-700">Current Address</label>
+                                <input
+                                    {...register("address")}
+                                    type="text"
+                                    name="address"
+                                    className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-primary-color"
+                                />
+                            </div>
+                            <div className="mb-4">
+                                <label className="block text-sm font-medium text-gray-700">Phone Number</label>
+                                <input
+                                    {...register("number")}
+                                    type="text"
+
+                                    name="number"
+                                    className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-primary-color"
+                                />
+                            </div>
+                            <button
+                                type="submit"
+                                className="px-4 py-2 text-white bg-primary-color rounded-md hover:bg-primary-color-dark focus:outline-none focus:ring"
+                            >
+                                Confirm Donation
+                            </button>
                         </div>
-                        <div className="mb-4">
-                            <label className="block text-sm font-medium text-gray-700">Current Address</label>
-                            <input
-                               {...register("address")}
-                               type="text"
-                               name="address"
-                               className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-primary-color"
-                            />
-                        </div>
-                        <div className="mb-4">
-                            <label className="block text-sm font-medium text-gray-700">Phone Number</label>
-                            <input
-                                   {...register("number")}
-                                   type="text"
-                        
-                                   name="number"
-                                   className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-primary-color"
-                            />
-                        </div>
-                        <button
-                            type="submit"
-                            className="px-4 py-2 text-white bg-primary-color rounded-md hover:bg-primary-color-dark focus:outline-none focus:ring"
-                        >
-                           Confirm Donation
-                        </button>
-                    </div>
-                </form>
-            </EditProfileModal>
-        </div>
+                    </form>
+                </EditProfileModal>
+            </div>
         </>
     );
 };
