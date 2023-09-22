f33d29f69596c1cbee5008ce7d1fc3e451e7d6d7
NISHAT TASNIM <tasnim.nt00@gmail.com>
2023-09-22 11:08:00 +0600

    doantion btn edit

--- src/components/HomePage/Donation/DonationCard.jsx
+++ src/components/HomePage/Donation/DonationCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { checkout } from '@/hooks/checkout';
 import Image from 'next/image';
+import HomeButton from '@/components/button/HomeButton';
 
 const DonationCard = ({ title, imageUrl, totalDonated, userProfileImage, username }) => {
 
@@ -16,57 +17,61 @@ const DonationCard = ({ title, imageUrl, totalDonated, userProfileImage, usernam
     };
 
     return (
-        <div className='mb-3'>
-            <div className="card glass rounded-md">
-                <Image
-                    src={imageUrl}
-                    alt="donate"
-                    layout="responsive"
-                    width={400}
-                    height={300}
-                    className="card-image rounded-md"
-                />
-                <div className="card-body">
-                    <h2 className="card-title">{title}</h2>
-                    
-                    {/* Display user profile picture and username */}
-                    <div className="flex items-center mt-3">
-                        <div className="w-8 h-8 rounded-full mr-2 overflow-hidden">
-                            <Image
-                                src={userProfileImage}
-                                alt="User Profile"
-                                layout="responsive"
-                                width={32}
-                                height={32}
-                            />
-                        </div>
-                        <span className="text-sm font-semibold">{username}</span>
-                    </div>
+			<div className="mb-3">
+				<div className="card glass rounded-md">
+					<Image
+						src={imageUrl}
+						alt="donate"
+						layout="responsive"
+						width={400}
+						height={300}
+						className="card-image rounded-md"
+					/>
+					<div className="card-body">
+						<h2 className="card-title">{title}</h2>
 
-                    <div className="card-actions mt-3">
-                        <div>
-                            {/* <div className="card-action">
+						{/* Display user profile picture and username */}
+						<div className="flex items-center mt-3">
+							<div className="w-8 h-8 rounded-full mr-2 overflow-hidden">
+								<Image
+									src={userProfileImage}
+									alt="User Profile"
+									layout="responsive"
+									width={32}
+									height={32}
+								/>
+							</div>
+							<span className="text-sm font-semibold">{username}</span>
+						</div>
+
+						<div className="card-actions mt-3">
+							<div>
+								{/* <div className="card-action">
                                 <button
                                     className="btn rounded-md hover:bg-primary-color hover:text-white border-gray font-semibold lg:ml-5 capitalize dark:border-white dark:hover:bg-black"
                                 >
                                     Total Donated {totalDonated}$
                                 </button>
                             </div> */}
-                            <div className="card-action">
-                                <button
+								<div className="card-action">
+									{/* <button
 
                                     onClick={handlePaymentSuccess}
                                     className="btn rounded-md hover:bg-primary-color hover:text-white border-gray font-semibold capitalize dark:border-white dark:hover:bg-black"
                                 >
                                     Donate now
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+                                </button> */}
+									<HomeButton
+										onClick={handlePaymentSuccess}
+										title="Donate now"
+									/>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
 };
 
 export default DonationCard;
--- src/components/HomePage/Donation/DonationCards.jsx
+++ src/components/HomePage/Donation/DonationCards.jsx
@@ -1,60 +1,60 @@
-"use client"
-import Link from "next/link"
+'use client';
+import Link from 'next/link';
 import React from 'react';
 import DonationCard from './DonationCard';
 import useSWR from 'swr';
+import HomeButton from '@/components/button/HomeButton';
 
 const DonationCards = () => {
+	const fetcher = (...args) => fetch(...args).then((res) => res.json());
+	const {
+		data: donationPosts,
+		error,
+		isLoading,
+	} = useSWR('/api/donation', fetcher, {
+		refreshInterval: 1000,
+	});
 
-  const fetcher = (...args) => fetch(...args).then((res) => res.json());
-  const { data: donationPosts, error, isLoading } = useSWR('/api/donation', fetcher, {
-    refreshInterval: 1000,
-  });
+	// Checking if there's an error or if the data is still loading
+	if (error) return <div>Failed to load</div>;
+	if (isLoading) return <div>Loading...</div>;
 
-  // Checking if there's an error or if the data is still loading
-  if (error) return <div>Failed to load</div>;
-  if (isLoading) return <div>Loading...</div>;
+	// Filter the donation posts based on criteria
 
-  // Filter the donation posts based on criteria
+	const approvedDonations =
+		donationPosts &&
+		donationPosts?.filter((donationPost) => donationPost.status === 'approved');
+	const displayDonation = approvedDonations.slice(0, 2);
 
-  const approvedDonations = donationPosts && donationPosts?.filter(donationPost => donationPost.status === 'approved');
-  const displayDonation = approvedDonations.slice(0, 2);
+	// console.log(displayDonation)
+	return (
+		// responsiveness
+		<>
+			<div className="">
+				{displayDonation.map((donation, index) => (
+					<DonationCard
+						key={index}
+						donation={donation}
+						title={donation?.content}
+						imageUrl={donation?.image}
+						totalDonated={donation?.amount}
+						userProfileImage={donation?.author?.profile_picture}
+						username={donation?.author?.name}
+					/>
+				))}
+			</div>
 
-  // console.log(displayDonation)
-  return (
-    // responsiveness
-<>
-    <div className="">
-      {
-
-          displayDonation.map((donation, index) => (
-            <DonationCard
-              key={index}
-              donation={donation}
-              title={donation?.content}
-              imageUrl={donation?.image}
-              totalDonated={donation?.amount}
-              userProfileImage={donation?.author?.profile_picture}
-              username={donation?.author?.name}
-
-            />
-          ))
-
-        }
-      </div>
-
-      <div className="flex justify-center">
-      <Link href="/allApprovedDonation">   <button
-        className="btn rounded-md hover:bg-primary-color hover:text-white border-gray font-semibold lg:ml-5 capitalize dark:border-white dark:hover:bg-black"
-      >
-        See more
-      </button></Link>
-      </div>
-    
-    </>
-
-
-  );
+			<div className="flex justify-center">
+				<Link href="/allApprovedDonation">
+					{' '}
+					{/* <button className="btn rounded-md hover:bg-primary-color hover:text-white border-gray font-semibold lg:ml-5 capitalize dark:border-white dark:hover:bg-black">
+						See more
+					</button> */}
+					<HomeButton title="See more" />
+				</Link>
+			</div>
+		</>
+	);
 };
 
-export default DonationCards;    
+export default DonationCards;
