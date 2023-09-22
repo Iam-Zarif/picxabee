5f6efa14ba37540be76f2c763c63ce2e1fd3fd82
NISHAT TASNIM <tasnim.nt00@gmail.com>
2023-09-22 09:38:02 +0600

    dashboardsidebar

--- src/app/dashboard/advertisements/page.js
+++ src/app/dashboard/advertisements/page.js
@@ -1,6 +1,7 @@
 'use client';
 import PendingAdCard from '@/components/Dashboard/advertisements/PendingAdCard';
 import useSWR from 'swr';
+import Loading from '../activities/loading';
 
 
 
@@ -15,26 +16,30 @@ const AdvertisementsPage = () => {
 	});
 
 	if (error) return <div>Failed to load</div>;
-	if (isLoading) return <div>Loading...</div>;
+	if (isLoading) return <div><Loading/> </div>;
 
 	
 
 	console.log(adPosts);
 
 	return (
-		<div className="flex flex-wrap">
-			{adPosts.map((adPost, index) => (
-				<div key={index} className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-2 mt-5">
-					<PendingAdCard
-						adPost={adPost}
-						// title={donationPost?.content}
-						// imageUrl={donationPost?.image}
-						// userProfileImage={donationPost?.author?.profile_picture}
-						// username={donationPost?.author?.name}
-					/>
-				</div>
-			))}
-		</div>
+		<>
+			{/* <div className="w-fit mx-auto mt-5 mb-10 ml-16 lg:ml-0">
+				<p className="text-primary-color font-bold lg:text-3xl text-xl text-center  border-b-2 px-3">
+					Pending advertisements
+				</p>
+			</div> */}
+			<div className="flex flex-wrap lg:w-10/12 justify-end lg:justify-center lg:p-0 pr-5 lg:mx-auto">
+				{adPosts.map((adPost, index) => (
+					<div
+						key={index}
+						className="w-9/12 sm:w-1/2 md:w-1/3 lg:w-1/4 py-2 lg:px-2 mt-5"
+					>
+						<PendingAdCard adPost={adPost} />
+					</div>
+				))}
+			</div>
+		</>
 	);
 };
 
